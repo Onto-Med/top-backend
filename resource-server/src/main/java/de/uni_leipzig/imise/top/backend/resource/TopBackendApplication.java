a880d542ebce5349a4eb2a6873b676e06b054040
Christoph Beger <ChristophB@users.noreply.github.com>
2021-11-22 22:38:07 +0100

    Also scan top-api package for spring components

--- resource-server/src/main/java/de/uni_leipzig/imise/top/backend/resource/TopBackendApplication.java
+++ resource-server/src/main/java/de/uni_leipzig/imise/top/backend/resource/TopBackendApplication.java
@@ -2,9 +2,11 @@ package de.uni_leipzig.imise.top.backend.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ComponentScan("de.uni_leipzig.imise.top.backend")
 @EnableTransactionManagement
 public class TopBackendApplication {
 
