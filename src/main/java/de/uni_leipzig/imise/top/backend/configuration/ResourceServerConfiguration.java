e54c9c7328c1a09d42d84cbd00ce3a7f732e23c7
Christoph Beger <ChristophB@users.noreply.github.com>
2021-11-22 13:19:09 +0100

    Add oauth2 resource server configurations

---
+++ src/main/java/de/uni_leipzig/imise/top/backend/configuration/ResourceServerConfiguration.java
@@ -0,0 +1,15 @@
+package de.uni_leipzig.imise.top.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class ResourceServerConfiguration {
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.oauth2ResourceServer().jwt();
+    return http.build();
+  }
+}
