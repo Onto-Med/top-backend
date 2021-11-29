32b0009f0a271a1424cd8e8bab66bb236b6f8d25
Christoph Beger <ChristophB@users.noreply.github.com>
2021-11-29 16:40:07 +0100

    Reuse organisation service and do refactor

--- resource-server/src/main/java/care/smith/top/backend/resource/api/OrganisationApiDelegateImpl.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/api/OrganisationApiDelegateImpl.java
@@ -2,8 +2,7 @@ package care.smith.top.backend.resource.api;
 
 import care.smith.top.backend.api.OrganisationApiDelegate;
 import care.smith.top.backend.model.Organisation;
-import care.smith.top.data.tables.records.DirectoryRecord;
-import org.jooq.DSLContext;
+import care.smith.top.backend.resource.service.OrganisationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,36 +10,14 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static care.smith.top.data.Tables.DIRECTORY;
-
 @Service
 public class OrganisationApiDelegateImpl implements OrganisationApiDelegate {
-  @Autowired DSLContext context;
+  @Autowired OrganisationService organisationService;
 
   @Override
   public ResponseEntity<Organisation> createOrganisation(
       Organisation organisation, List<String> include) {
-    // TODO: use below code to get current user
-    // UserDetails userDetails =
-    //   (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    //
-    // throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, userDetails.getUsername());
-
-    DirectoryRecord newRecord =
-        context
-            .newRecord(DIRECTORY)
-            .setName(organisation.getName())
-            .setDescription(organisation.getDescription())
-            .setType("organisation");
-    newRecord.store();
-    newRecord.refresh();
-
-    Organisation created = new Organisation();
-    created.setOrganisationId(newRecord.getDirectoryId());
-    created.setName(newRecord.getName());
-    created.setDescription(newRecord.getDescription());
-    created.setCreatedAt(newRecord.getCreatedAt());
-
-    return new ResponseEntity<>(created, HttpStatus.CREATED);
+    return new ResponseEntity<>(
+        organisationService.createOrganisation(organisation), HttpStatus.CREATED);
   }
 }
--- resource-server/src/main/java/care/smith/top/backend/resource/api/OrganisationNameApiDelegateImpl.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/api/OrganisationNameApiDelegateImpl.java
@@ -3,40 +3,40 @@ package care.smith.top.backend.resource.api;
 import care.smith.top.backend.api.OrganisationNameApiDelegate;
 import care.smith.top.backend.model.Entity;
 import care.smith.top.backend.model.Organisation;
-import care.smith.top.data.tables.records.DirectoryRecord;
-import org.jooq.DSLContext;
+import care.smith.top.backend.resource.service.OrganisationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
-import static care.smith.top.data.Tables.*;
+import java.util.UUID;
 
 @Service
 public class OrganisationNameApiDelegateImpl implements OrganisationNameApiDelegate {
-  @Autowired
-  DSLContext context;
+  @Autowired OrganisationService organisationService;
+
   @Override
-  public ResponseEntity<Entity> createEntity(String organisationName, String repositoryName, Entity entity, List<String> include) {
+  public ResponseEntity<Entity> createEntity(
+      String organisationName, String repositoryName, Entity entity, List<String> include) {
     return new ResponseEntity<>(entity, HttpStatus.CREATED);
   }
 
   @Override
-  public ResponseEntity<Organisation> deleteOrganisationByName(String organisationName, List<String> include) {
-    DirectoryRecord record = context.fetchOne(DIRECTORY, DIRECTORY.NAME.eq(organisationName));
-    if (record == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+  public ResponseEntity<Entity> getEntityById(
+      String organisationName,
+      String repositoryName,
+      UUID id,
+      Integer version,
+      List<String> include) {
+    return OrganisationNameApiDelegate.super.getEntityById(
+        organisationName, repositoryName, id, version, include);
+  }
 
-    Organisation response = new Organisation();
-    response.setOrganisationId(record.getDirectoryId());
-    response.setName(record.getName());
-    response.setDescription(record.getDescription());
-    response.setCreatedAt(record.getCreatedAt());
-
-    record.delete();
-
-    return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+  @Override
+  public ResponseEntity<Organisation> deleteOrganisationByName(
+      String organisationName, List<String> include) {
+    organisationService.deleteOrganisationByName(organisationName);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
