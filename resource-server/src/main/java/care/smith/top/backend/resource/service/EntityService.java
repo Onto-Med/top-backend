7bb95734ab7f91f9fc5383a1848a3e2f280930d6
ChristophB <christoph.beger@medizin.uni-leipzig.de>
2021-11-30 14:55:31 +0100

    Refactor load of entity record

--- resource-server/src/main/java/care/smith/top/backend/resource/service/EntityService.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/service/EntityService.java
@@ -1,7 +1,7 @@
 package care.smith.top.backend.resource.service;
 
 import care.smith.top.backend.model.Entity;
-import care.smith.top.data.tables.records.ClassRecord;
+import care.smith.top.data.tables.records.ClassVersionRecord;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,7 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
-import static care.smith.top.data.Tables.CLASS;
+import static care.smith.top.data.Tables.CLASS_VERSION;
 
 @Service
 public class EntityService {
@@ -18,17 +18,30 @@ public class EntityService {
 
   public Entity loadEntity(
       String organisationName, String repositoryName, UUID id, Integer version) {
-    ClassRecord record = context.fetchOne(CLASS, CLASS.UUID.eq(id));
+    ClassVersionRecord record = loadEntityRecord(organisationName, repositoryName, id, version);
     if (record == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
     return mapToEntity(record);
   }
 
-  private Entity mapToEntity(ClassRecord record) {
+  public void deleteEntity(
+      String organisationName, String repositoryName, UUID id, Integer version) {
+    ClassVersionRecord record = loadEntityRecord(organisationName, repositoryName, id, version);
+    if (record == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    record.delete();
+  }
+
+  private Entity mapToEntity(ClassVersionRecord record) {
     Entity entity = new Entity();
-    entity.setId(record.getUuid());
     // TODO: implement mapping from DB...
 
     return entity;
+  }
+
+  private ClassVersionRecord loadEntityRecord(
+      String organisationName, String repositoryName, UUID id, Integer version) {
+    // TODO: filter by organisationName and repositoryName
+    return context.fetchOne(
+        CLASS_VERSION, CLASS_VERSION.class_().UUID.eq(id).and(CLASS_VERSION.VERSION.eq(version)));
   }
 }
