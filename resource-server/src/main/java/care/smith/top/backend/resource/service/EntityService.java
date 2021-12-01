9da443421ac81be52bc70d237cc5a6be66934e45
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2021-12-01 14:15:36 +0100

    Add example on how to fetch annotations from db

--- resource-server/src/main/java/care/smith/top/backend/resource/service/EntityService.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/service/EntityService.java
@@ -1,8 +1,14 @@
 package care.smith.top.backend.resource.service;
 
 import care.smith.top.backend.model.Entity;
+import care.smith.top.backend.model.LocalisableText;
+import care.smith.top.data.Keys;
+import care.smith.top.data.tables.records.AnnotationRecord;
+import care.smith.top.data.tables.records.ClassRecord;
 import care.smith.top.data.tables.records.ClassVersionRecord;
+import care.smith.top.data.tables.records.PropertyRecord;
 import org.jooq.DSLContext;
+import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -10,8 +16,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
-import static care.smith.top.data.Tables.CLASS_VERSION;
+import static care.smith.top.data.Tables.*;
 
 @Service
 public class EntityService {
@@ -35,7 +42,59 @@ public class EntityService {
 
   private Entity mapToEntity(ClassVersionRecord record) {
     Entity entity = new Entity();
+
     // TODO: implement mapping from DB...
+    ClassRecord classRecord = record.fetchParent(Keys.CLASS_VERSION__FK_CLASS_TO_CLASS_VERSION);
+    if (classRecord == null)
+      throw new ResponseStatusException(
+          HttpStatus.INTERNAL_SERVER_ERROR,
+          String.format("Class with ID '%d' does not exist.", record.getClassId()));
+
+    entity.setId(classRecord.getUuid());
+    entity.setCreatedAt(record.getCreatedAt());
+    entity.setVersion(record.getVersion());
+
+    /**** Examples on how to get annotation "title" ***
+     * TODO: select one approach and delete the other one
+     */
+
+    /** Via fetchChildren(): * */
+    Result<AnnotationRecord> annotations =
+        record.fetchChildren(Keys.ANNOTATION__FK_CLASS_VERSION_TO_ANNOTATION);
+    PropertyRecord titleProperty = context.fetchOne(PROPERTY, PROPERTY.NAME.eq("title"));
+
+    if (titleProperty != null)
+      entity.setTitles(
+          annotations.stream()
+              .filter(a -> a.getPropertyId().equals(titleProperty.getPropertyId()))
+              .map(
+                  a -> {
+                    LocalisableText title = new LocalisableText();
+                    title.setLang(a.getLanguage());
+                    title.setText(a.getStringValue());
+                    return title;
+                  })
+              .collect(Collectors.toList()));
+
+    /** Via fresh query * */
+    Result<AnnotationRecord> titles =
+        context.fetch(
+            ANNOTATION,
+            ANNOTATION
+                .CLASS_VERSION_ID
+                .eq(record.getClassId())
+                .and(ANNOTATION.property().NAME.eq("title")));
+
+    entity.setTitles(
+        titles.stream()
+            .map(
+                t -> {
+                  LocalisableText title = new LocalisableText();
+                  title.setLang(t.getLanguage());
+                  title.setText(t.getStringValue());
+                  return title;
+                })
+            .collect(Collectors.toList()));
 
     return entity;
   }
