8c1e0b452c35d28b5ab64bfa872c03383f5ee8ab
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-01-26 14:42:46 +0100

    Add query to get sorted subclasses in repository

--- neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/ClassRepository.java
+++ neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/ClassRepository.java
@@ -1,11 +1,18 @@
 package care.smith.top.backend.neo4j_ontology_access.repository;
 
 import care.smith.top.backend.neo4j_ontology_access.model.Class;
+import care.smith.top.backend.neo4j_ontology_access.model.Repository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
-@Repository
+@org.springframework.stereotype.Repository
 public interface ClassRepository extends PagingAndSortingRepository<Class, UUID> {
+  @Query(
+      "MATCH (super:Class {uuid: $cls.uuid}) <-[:HAS_SUPERCLASS]- (cr:ClassRelation {id: $repository.id}) "
+          + "MATCH (cr) <-[:IS_SUBCLASS_OF]- (sub:Class) "
+          + "RETURN sub ORDER BY index")
+  Stream<Class> findSubclasses(Class cls, Repository repository);
 }
