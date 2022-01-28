8c16885948cbacec2f5c735cb32cfdf524a1a7b5
Christoph Beger <ChristophB@users.noreply.github.com>
2022-01-28 18:34:39 +0100

    Add method to get repository by id and super directory id

--- neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/RepositoryRepository.java
+++ neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/RepositoryRepository.java
@@ -1,9 +1,19 @@
 package care.smith.top.backend.neo4j_ontology_access.repository;
 
 import care.smith.top.backend.neo4j_ontology_access.model.Repository;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Repository
 public interface RepositoryRepository extends PagingAndSortingRepository<Repository, String> {
 
+  @Query(
+      "MATCH (r:Repository { id: $repositoryId }) -[:BELONGS_TO]-> (d:Directory { id: $superDirectoryId })"
+          + "RETURN r")
+  Optional<Repository> findByIdAndSuperDirectoryId(
+      @Param("repositoryId") String repositoryId,
+      @Param("superDirectoryId") String superDirectoryId);
 }
