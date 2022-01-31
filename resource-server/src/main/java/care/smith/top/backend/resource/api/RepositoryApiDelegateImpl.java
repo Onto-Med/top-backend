e66d6e9b219c7e61fbb11c3f0bca542390ebd6e1
Christoph Beger <ChristophB@users.noreply.github.com>
2022-01-31 15:42:44 +0100

    Implement creation of repositories

--- resource-server/src/main/java/care/smith/top/backend/resource/api/RepositoryApiDelegateImpl.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/api/RepositoryApiDelegateImpl.java
@@ -2,6 +2,9 @@ package care.smith.top.backend.resource.api;
 
 import care.smith.top.backend.api.RepositoryApiDelegate;
 import care.smith.top.backend.model.Repository;
+import care.smith.top.backend.resource.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -9,33 +12,43 @@ import java.util.List;
 
 @Service
 public class RepositoryApiDelegateImpl implements RepositoryApiDelegate {
+  @Autowired RepositoryService repositoryService;
+
   @Override
-  public ResponseEntity<Repository> createRepository(String organisationId, Repository repository, List<String> include) {
-    return RepositoryApiDelegate.super.createRepository(organisationId, repository, include);
+  public ResponseEntity<Repository> createRepository(
+      String organisationId, Repository repository, List<String> include) {
+    return new ResponseEntity<>(repositoryService.createRepository(organisationId, repository, include), HttpStatus.CREATED);
   }
 
   @Override
-  public ResponseEntity<Void> deleteRepositoryById(String repositoryId, String organisationId, List<String> include) {
+  public ResponseEntity<Void> deleteRepositoryById(
+      String repositoryId, String organisationId, List<String> include) {
     return RepositoryApiDelegate.super.deleteRepositoryById(repositoryId, organisationId, include);
   }
 
   @Override
-  public ResponseEntity<List<Repository>> getRepositories(List<String> include, String name, Integer page) {
+  public ResponseEntity<List<Repository>> getRepositories(
+      List<String> include, String name, Integer page) {
     return RepositoryApiDelegate.super.getRepositories(include, name, page);
   }
 
   @Override
-  public ResponseEntity<List<Repository>> getRepositoriesByOrganisationId(String organisationId, List<String> include, String name, Integer page) {
-    return RepositoryApiDelegate.super.getRepositoriesByOrganisationId(organisationId, include, name, page);
+  public ResponseEntity<List<Repository>> getRepositoriesByOrganisationId(
+      String organisationId, List<String> include, String name, Integer page) {
+    return RepositoryApiDelegate.super.getRepositoriesByOrganisationId(
+        organisationId, include, name, page);
   }
 
   @Override
-  public ResponseEntity<Repository> getRepositoryById(String organisationId, String repositoryId, List<String> include) {
+  public ResponseEntity<Repository> getRepositoryById(
+      String organisationId, String repositoryId, List<String> include) {
     return RepositoryApiDelegate.super.getRepositoryById(organisationId, repositoryId, include);
   }
 
   @Override
-  public ResponseEntity<Repository> updateRepositoryById(String organisationId, String repositoryId, Repository repository, List<String> include) {
-    return RepositoryApiDelegate.super.updateRepositoryById(organisationId, repositoryId, repository, include);
+  public ResponseEntity<Repository> updateRepositoryById(
+      String organisationId, String repositoryId, Repository repository, List<String> include) {
+    return RepositoryApiDelegate.super.updateRepositoryById(
+        organisationId, repositoryId, repository, include);
   }
 }
---
+++ resource-server/src/main/java/care/smith/top/backend/resource/service/RepositoryService.java
@@ -0,0 +1,64 @@
+package care.smith.top.backend.resource.service;
+
+import care.smith.top.backend.model.Organisation;
+import care.smith.top.backend.neo4j_ontology_access.model.Directory;
+import care.smith.top.backend.neo4j_ontology_access.model.Repository;
+import care.smith.top.backend.neo4j_ontology_access.repository.DirectoryRepository;
+import care.smith.top.backend.neo4j_ontology_access.repository.RepositoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class RepositoryService {
+  @Autowired private RepositoryRepository repositoryRepository;
+  @Autowired private DirectoryRepository directoryRepository;
+
+  @Transactional
+  public care.smith.top.backend.model.Repository createRepository(
+      String organisationId, care.smith.top.backend.model.Repository data, List<String> include) {
+    Directory organisation = getOrganisation(organisationId);
+
+    if (repositoryRepository.existsById(data.getId()))
+      throw new ResponseStatusException(HttpStatus.CONFLICT);
+
+    // TODO: let users set 'primary' field of repositories
+    Repository repository =
+        (Repository)
+            new Repository(data.getId())
+                .setName(data.getName())
+                .setDescription(data.getDescription())
+                .addSuperDirectory(organisation); // TODO: super directory not stored in db!
+
+    return repositoryToApiPojo(repositoryRepository.save(repository));
+  }
+
+  /**
+   * Convert {@link Repository} object to {@link care.smith.top.backend.model.Repository} object.
+   *
+   * @param repository The object to be converted.
+   * @return The resulting {@link care.smith.top.backend.model.Repository} object.
+   */
+  private care.smith.top.backend.model.Repository repositoryToApiPojo(Repository repository) {
+    return new care.smith.top.backend.model.Repository()
+        .createdAt(repository.getCreatedAtOffset())
+        .id(repository.getId())
+        .name(repository.getName())
+        .description(repository.getDescription())
+        .organisation(new Organisation());
+  }
+
+  private Directory getOrganisation(String organisationId) {
+    return directoryRepository
+        .findById(organisationId)
+        .orElseThrow(
+            () ->
+                new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    String.format("Organisation '%s' does not exist!", organisationId)));
+  }
+}
