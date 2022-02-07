9703c218104ce401840b6cd70c9de5b492fa347c
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-07 17:28:23 +0100

    Add test for create of restricted phenotype

--- resource-server/src/test/java/care/smith/top/backend/resource/service/EntityServiceTest.java
+++ resource-server/src/test/java/care/smith/top/backend/resource/service/EntityServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -65,7 +66,7 @@ class EntityServiceTest extends Neo4jTest {
               assertThat(c.getRepository())
                   .isNotNull()
                   .hasFieldOrPropertyWithValue("id", repository.getId());
-              assertThat(((Category) c).getSuperCategories()).isNull();
+              assertThat(((Category) c).getSuperCategories()).isNullOrEmpty();
             });
 
     assertThatThrownBy(
@@ -74,26 +75,79 @@ class EntityServiceTest extends Neo4jTest {
         .hasFieldOrPropertyWithValue("status", HttpStatus.CONFLICT);
 
     /* Create abstract phenotype */
-    Phenotype phenotype = new Phenotype();
-    phenotype
+    Phenotype abstractPhenotype = new Phenotype().addUnitsItem(new Unit().unit("cm"));
+    abstractPhenotype
         .addSuperCategoriesItem(category)
         .id(UUID.randomUUID())
         .entityType(EntityType.SINGLE_PHENOTYPE);
 
-    assertThat(entityService.createEntity(organisation.getId(), repository.getId(), phenotype))
+    assertThat(
+            entityService.createEntity(organisation.getId(), repository.getId(), abstractPhenotype))
         .isNotNull()
         .isInstanceOf(Phenotype.class)
         .satisfies(
             p -> {
-              assertThat(p.getId()).isEqualTo(phenotype.getId());
+              assertThat(p.getId()).isEqualTo(abstractPhenotype.getId());
               assertThat(p.getVersion()).isEqualTo(1);
-              assertThat(p.getEntityType()).isEqualTo(phenotype.getEntityType());
+              assertThat(p.getEntityType()).isEqualTo(abstractPhenotype.getEntityType());
               assertThat(((Phenotype) p).getSuperPhenotype()).isNull();
               assertThat(((Phenotype) p).getSuperCategories())
                   .allMatch(c -> c.getId().equals(category.getId()));
+              assertThat(((Phenotype) p).getUnits()).allMatch(u -> u.getUnit().equals("cm"));
             });
 
     /* Create restricted phenotype */
+    Phenotype restrictedPhenotype =
+        new Phenotype()
+            .score(BigDecimal.valueOf(10))
+            .restriction(
+                new NumberRestriction()
+                    .addValuesItem(BigDecimal.valueOf(50))
+                    .minOperator(RestrictionOperator.GREATER_THAN)
+                    .quantor(Quantor.SOME)
+                    .type(DataType.NUMBER));
+    restrictedPhenotype
+        .superPhenotype(abstractPhenotype)
+        .id(UUID.randomUUID())
+        .entityType(EntityType.SINGLE_RESTRICTION)
+        .addTitlesItem(new LocalisableText().text("> 50cm").lang("en"));
+
+    assertThat(
+            entityService.createEntity(
+                organisation.getId(), repository.getId(), restrictedPhenotype))
+        .isNotNull()
+        .isInstanceOf(Phenotype.class)
+        .satisfies(
+            rp -> {
+              assertThat(rp.getId()).isEqualTo(restrictedPhenotype.getId());
+              assertThat(rp.getVersion()).isEqualTo(1);
+              assertThat(rp.getEntityType()).isEqualTo(EntityType.SINGLE_RESTRICTION);
+              assertThat(((Phenotype) rp).getSuperPhenotype())
+                  .hasFieldOrPropertyWithValue("id", abstractPhenotype.getId());
+              assertThat(rp.getTitles())
+                  .allMatch(t -> t.getText().equals("> 50cm") && t.getLang().equals("en"))
+                  .size()
+                  .isEqualTo(1);
+              assertThat(rp.getRepository()).hasFieldOrPropertyWithValue("id", repository.getId());
+              assertThat(((Phenotype) rp).getSuperCategories()).isNullOrEmpty();
+              assertThat(((Phenotype) rp).getScore().compareTo(BigDecimal.valueOf(10)))
+                  .isEqualTo(0);
+              assertThat(((Phenotype) rp).getRestriction())
+                  .isNotNull()
+                  .isInstanceOf(NumberRestriction.class)
+                  .satisfies(
+                      r -> {
+                        assertThat(r.getQuantor()).isEqualTo(Quantor.SOME);
+                        assertThat(((NumberRestriction) r).getMaxOperator()).isNull();
+                        assertThat(((NumberRestriction) r).getMinOperator())
+                            .isNotNull()
+                            .isEqualTo(RestrictionOperator.GREATER_THAN);
+                        assertThat(((NumberRestriction) r).getValues())
+                            .allMatch(v -> v.compareTo(BigDecimal.valueOf(50)) == 0)
+                            .size()
+                            .isEqualTo(1);
+                      });
+            });
   }
 
   @Test
