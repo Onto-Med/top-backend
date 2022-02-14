67ac1f0013eaa86ccaa1f1a50eead6488046293e
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-14 15:53:15 +0100

    Include super directory in query result

--- neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepository.java
+++ neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepository.java
@@ -9,14 +9,25 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DirectoryRepository extends PagingAndSortingRepository<Directory, String> {
-  Streamable<Directory> findByNameContainingIgnoreCase(String name);
-
-  Streamable<Directory> findByDescriptionContainingIgnoreCase(String description);
+  @Query(
+      "MATCH (d:Directory) "
+          + "WHERE d.name =~ '(?i).*' + $name + '.*' "
+          + "OPTIONAL MATCH p = (d) -[:BELONGS_TO]-> (:Directory) "
+          + "RETURN d, relationships(p), nodes(p)")
+  Streamable<Directory> findByNameContainingIgnoreCase(@Param("name") String name);
 
   @Query(
-    "MATCH (d:Directory) "
-    + "WHERE $type IN labels(d) "
-    + "RETURN d"
-  )
+      "MATCH (d:Directory) "
+          + "WHERE d.description =~ '(?i).*' + $description + '.*' "
+          + "OPTIONAL MATCH p = (d) -[:BELONGS_TO]-> (:Directory) "
+          + "RETURN d, relationships(p), nodes(p)")
+  Streamable<Directory> findByDescriptionContainingIgnoreCase(
+      @Param("description") String description);
+
+  @Query(
+      "MATCH (d:Directory) "
+          + "WHERE $type IN labels(d) "
+          + "OPTIONAL MATCH p = (d) -[:BELONGS_TO]-> (:Directory) "
+          + "RETURN d, relationships(p), nodes(p)")
   Streamable<Directory> findByType(@Param("type") String type);
 }
