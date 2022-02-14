5cadc8f55346a73cfa2ce8b8b8cce18a96d9fd68
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-14 11:43:17 +0100

    Add query to filter directories by type

--- neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepository.java
+++ neo4j-ontology-access/src/main/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepository.java
@@ -1,7 +1,9 @@
 package care.smith.top.backend.neo4j_ontology_access.repository;
 
 import care.smith.top.backend.neo4j_ontology_access.model.Directory;
+import org.springframework.data.neo4j.repository.query.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,11 @@ public interface DirectoryRepository extends PagingAndSortingRepository<Director
   Streamable<Directory> findByNameContainingIgnoreCase(String name);
 
   Streamable<Directory> findByDescriptionContainingIgnoreCase(String description);
+
+  @Query(
+    "MATCH (d:Directory) "
+    + "WHERE $type IN labels(d) "
+    + "RETURN d"
+  )
+  Streamable<Directory> findByType(@Param("type") String type);
 }
--- neo4j-ontology-access/src/test/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepositoryTest.java
+++ neo4j-ontology-access/src/test/java/care/smith/top/backend/neo4j_ontology_access/repository/DirectoryRepositoryTest.java
@@ -3,9 +3,11 @@ package care.smith.top.backend.neo4j_ontology_access.repository;
 import care.smith.top.backend.neo4j_ontology_access.model.Directory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,8 +25,20 @@ class DirectoryRepositoryTest extends RepositoryTest {
             new Directory().setName(name1 + "1"),
             new Directory().setName(name2)));
 
-    assertThat(directoryRepository.findAll(Pageable.ofSize(10)).getTotalElements()).isEqualTo(3);
+    assertThat(directoryRepository.findAll(Pageable.ofSize(10))).size().isEqualTo(3);
     assertThat(directoryRepository.findByNameContainingIgnoreCase(name1)).size().isEqualTo(2);
     assertThat(directoryRepository.findByDescriptionContainingIgnoreCase("desc")).size().isEqualTo(1);
+  }
+
+  @Test
+  void findByType() {
+    directoryRepository.saveAll(
+      Arrays.asList(
+        new Directory().setTypes(Collections.singleton("Organisation")),
+        new Directory().setTypes(Collections.singleton("Project"))
+      )
+    );
+
+    assertThat(directoryRepository.findByType("Organisation")).size().isEqualTo(1);
   }
 }
