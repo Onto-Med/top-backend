385ab03bf1191a7cccda9aefdd4cc4bd0fc386ce
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-07-22 10:54:39 +0200

    Load functions and constants from simple-onto-api

--- resource-server/src/main/java/care/smith/top/backend/resource/api/ExpressionConstantApiDelegateImpl.java
+++ resource-server/src/main/java/care/smith/top/backend/resource/api/ExpressionConstantApiDelegateImpl.java
@@ -2,23 +2,27 @@ package care.smith.top.backend.resource.api;
 
 import care.smith.top.backend.api.ExpressionConstantApiDelegate;
 import care.smith.top.backend.model.Constant;
+import care.smith.top.backend.resource.util.OntoModelMapper;
+import care.smith.top.simple_onto_api.calculator.Calculator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ExpressionConstantApiDelegateImpl implements ExpressionConstantApiDelegate {
+  private Calculator calculator = new Calculator();
+
   @Override
   public ResponseEntity<List<Constant>> getConstants() {
     return new ResponseEntity<>(
-        Arrays.asList(
-            new Constant().id("e"),
-            new Constant().id("pi").title("π"),
-            new Constant().id("true").title("True"),
-            new Constant().id("false").title("False")),
+        calculator.getConstants().stream()
+            .map(OntoModelMapper::map)
+            .sorted(Comparator.comparing(Constant::getTitle))
+            .collect(Collectors.toList()),
         HttpStatus.OK);
   }
 }
