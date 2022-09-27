9e678d0d3cf1d01a087913fe25640102e8ccfa47
Christoph Beger <ChristophB@users.noreply.github.com>
2022-09-27 10:47:05 +0200

    Remove unused imports

--- src/main/java/care/smith/top/backend/TopBackendApplication.java
+++ src/main/java/care/smith/top/backend/TopBackendApplication.java
@@ -4,15 +4,10 @@ import care.smith.top.backend.configuration.InfrastructureConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.time.OffsetDateTime;
-import java.util.Optional;
 
 @SpringBootApplication
 @Import(InfrastructureConfig.class)
