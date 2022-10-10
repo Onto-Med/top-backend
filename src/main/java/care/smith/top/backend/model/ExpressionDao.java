e001925eee646841302f96f917241dbcf402e5b5
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-10-10 11:38:49 +0200

    Make functionId of ExpressionDao optional

--- src/main/java/care/smith/top/backend/model/ExpressionDao.java
+++ src/main/java/care/smith/top/backend/model/ExpressionDao.java
@@ -15,7 +15,6 @@ import java.util.stream.Collectors;
 public class ExpressionDao {
   @Id @GeneratedValue private Long id;
 
-  @Column(nullable = false)
   private String functionId;
 
   private String entityId;
