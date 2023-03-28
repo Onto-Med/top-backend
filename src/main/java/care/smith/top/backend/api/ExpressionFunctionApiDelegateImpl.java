78cf446579576972f58329de63a23c54bbd029c5
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2023-03-28 13:04:12 +0200

    Do not exclude `switch` from list of available functions

--- src/main/java/care/smith/top/backend/api/ExpressionFunctionApiDelegateImpl.java
+++ src/main/java/care/smith/top/backend/api/ExpressionFunctionApiDelegateImpl.java
@@ -3,21 +3,17 @@ package care.smith.top.backend.api;
 import care.smith.top.model.ExpressionFunction;
 import care.smith.top.top_phenotypic_query.c2reasoner.C2R;
 import care.smith.top.top_phenotypic_query.c2reasoner.functions.FunctionEntity;
-import care.smith.top.top_phenotypic_query.c2reasoner.functions.advanced.Switch;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class ExpressionFunctionApiDelegateImpl implements ExpressionFunctionApiDelegate {
-  public static final List<String> EXCLUDED_FUNCTION_IDS =
-      Collections.singletonList(Switch.get().getFunctionId());
   private final C2R calculator = new C2R();
 
   @Override
@@ -26,7 +22,6 @@ public class ExpressionFunctionApiDelegateImpl implements ExpressionFunctionApiD
         calculator.getFunctions().stream()
             .filter(f -> StringUtils.isBlank(type) || type.equals(f.getType()))
             .map(FunctionEntity::getFunction)
-            .filter(f -> !EXCLUDED_FUNCTION_IDS.contains(f.getId()))
             .sorted(Comparator.comparing(ExpressionFunction::getTitle))
             .collect(Collectors.toList()),
         HttpStatus.OK);
