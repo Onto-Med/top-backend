1595e2a01c6a80efd20c3db6215c41fb120f3fde
Christoph Beger <ChristophB@users.noreply.github.com>
2023-04-17 13:46:30 +0200

    Store query results in file system

--- src/main/java/care/smith/top/backend/service/PhenotypeQueryService.java
+++ src/main/java/care/smith/top/backend/service/PhenotypeQueryService.java
@@ -10,8 +10,26 @@ import care.smith.top.backend.util.ApiModelMapper;
 import care.smith.top.model.*;
 import care.smith.top.top_phenotypic_query.adapter.DataAdapter;
 import care.smith.top.top_phenotypic_query.adapter.config.DataAdapterConfig;
+import care.smith.top.top_phenotypic_query.converter.csv.CSV;
 import care.smith.top.top_phenotypic_query.result.ResultSet;
 import care.smith.top.top_phenotypic_query.search.PhenotypeFinder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystemException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.jetbrains.annotations.NotNull;
 import org.jobrunr.jobs.Job;
 import org.jobrunr.jobs.states.StateName;
@@ -28,19 +46,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.*;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 @Service
 @Transactional
 public class PhenotypeQueryService {
   private static final Logger LOGGER = Logger.getLogger(PhenotypeQueryService.class.getName());
+
+  private final CSV csvConverter = new CSV();
 
   @Value("${spring.paging.page-size:10}")
   private int pageSize;
@@ -50,6 +61,9 @@ public class PhenotypeQueryService {
 
   @Value("${top.phenotyping.execute-queries:true}")
   private boolean executeQueries;
+
+  @Value("${top.phenotyping.result-dir:config/query_results}")
+  private String resultDir;
 
   @Autowired private JobScheduler jobScheduler;
   @Autowired private StorageProvider storageProvider;
@@ -68,7 +82,6 @@ public class PhenotypeQueryService {
             .findByRepository_OrganisationIdAndRepositoryIdAndId(
                 organisationId, repositoryId, queryId.toString())
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    queryRepository.delete(query);
 
     try {
       Job job = storageProvider.getJobById(queryId);
@@ -76,6 +89,17 @@ public class PhenotypeQueryService {
     } catch (Exception e) {
       LOGGER.fine(e.getMessage());
     }
+
+    try {
+      clearResult(
+          query.getRepository().getOrganisation().getId(),
+          query.getRepository().getId(),
+          queryId.toString());
+    } catch (IOException e) {
+      LOGGER.warning(e.getMessage());
+    }
+
+    queryRepository.delete(query);
   }
 
   @PreAuthorize(
@@ -136,28 +160,36 @@ public class PhenotypeQueryService {
     DataAdapterConfig config = configs.stream().findFirst().orElseThrow();
 
     QueryResultDao result;
-    if (executeQueries) {
-      try {
+    try {
+      ResultSet rs;
+      if (executeQueries) {
         DataAdapter adapter = DataAdapter.getInstance(config);
         if (adapter == null) throw new NullPointerException("Adaptor type could not be derived.");
 
-        // TODO: provide Writer to top-phenotypic-query and let it store the result set
         PhenotypeFinder finder = new PhenotypeFinder(query, phenotypes, adapter);
-        ResultSet rs = finder.execute();
+        rs = finder.execute();
         adapter.close();
         result =
             new QueryResultDao(
                 queryDao, createdAt, (long) rs.size(), OffsetDateTime.now(), QueryState.FINISHED);
-      } catch (Throwable e) {
-        e.printStackTrace();
+      } else {
+        rs = new ResultSet();
         result =
-            new QueryResultDao(queryDao, createdAt, null, OffsetDateTime.now(), QueryState.FAILED)
-                .message("Cause: " + (e.getMessage() != null ? e.getMessage() : e.toString()));
+            new QueryResultDao(queryDao, createdAt, 0L, OffsetDateTime.now(), QueryState.FINISHED)
+                .message("Query execution is disabled.");
       }
-    } else {
+
+      storeResult(
+          queryDao.getRepository().getOrganisation().getId(),
+          queryDao.getRepository().getId(),
+          queryId.toString(),
+          rs,
+          phenotypes);
+    } catch (Throwable e) {
+      e.printStackTrace();
       result =
-          new QueryResultDao(queryDao, createdAt, 0L, OffsetDateTime.now(), QueryState.FINISHED)
-              .message("Query execution is disabled.");
+          new QueryResultDao(queryDao, createdAt, null, OffsetDateTime.now(), QueryState.FAILED)
+              .message("Cause: " + (e.getMessage() != null ? e.getMessage() : e.toString()));
     }
 
     queryDao.result(result);
@@ -227,6 +259,16 @@ public class PhenotypeQueryService {
         .map(QueryDao::toApiModel);
   }
 
+  private void clearResult(String organisationId, String repositoryId, String queryId)
+      throws IOException {
+    Path resultPath =
+        Paths.get(resultDir, organisationId, repositoryId, String.format("%s.zip", queryId));
+    if (!resultPath.startsWith(Paths.get(resultDir)))
+      LOGGER.severe(
+          String.format("Could not delete query results of %s from file system.", queryId));
+    Files.deleteIfExists(resultPath);
+  }
+
   @NotNull
   private List<DataAdapterConfig> getConfigs(List<String> dataSources) {
     return dataSources.stream()
@@ -244,6 +286,31 @@ public class PhenotypeQueryService {
       return QueryState.RUNNING;
     }
     return QueryState.QUEUED;
+  }
+
+  private void storeResult(
+      String organisationId,
+      String repositoryId,
+      String queryId,
+      ResultSet resultSet,
+      Entity[] phenotypes)
+      throws IOException {
+    Path repositoryPath = Paths.get(resultDir, organisationId, repositoryId);
+    if (!repositoryPath.startsWith(Paths.get(resultDir)))
+      throw new FileSystemException("Repository directory isn't a child of the results directory.");
+
+    Files.createDirectories(repositoryPath);
+    File zipFile =
+        Files.createFile(repositoryPath.resolve(String.format("%s.zip", queryId))).toFile();
+    ZipOutputStream zipStream = new ZipOutputStream(new FileOutputStream(zipFile));
+
+    zipStream.putNextEntry(new ZipEntry("data.csv"));
+    csvConverter.write(resultSet, zipStream);
+
+    zipStream.putNextEntry(new ZipEntry("metadata.csv"));
+    csvConverter.write(phenotypes, zipStream);
+
+    zipStream.close();
   }
 
   private DataAdapterConfig toDataAdapterConfig(Path path) {
