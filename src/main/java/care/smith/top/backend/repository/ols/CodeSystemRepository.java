6614e0d6aad138d8b1537357140c57089950a446
Christoph Beger <ChristophB@users.noreply.github.com>
2023-05-26 20:35:54 +0200

    Catch exception if code system could not be loaded from OLS

--- src/main/java/care/smith/top/backend/repository/ols/CodeSystemRepository.java
+++ src/main/java/care/smith/top/backend/repository/ols/CodeSystemRepository.java
@@ -5,41 +5,50 @@ import care.smith.top.model.CodeSystem;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CodeSystemRepository extends OlsRepository {
+  private static final Logger LOGGER = Logger.getLogger(CodeSystemRepository.class.getName());
+
   @Cacheable("olsOntologies")
   public Map<URI, CodeSystem> getAllCodeSystems() {
-    OLSOntologiesResponse response =
-        terminologyService
-            .get()
-            .uri(
-                uriBuilder ->
-                    uriBuilder
-                        .path("/ontologies")
-                        .queryParam("page", 0)
-                        .queryParam("size", 1000)
-                        .build())
-            .retrieve()
-            .bodyToMono(OLSOntologiesResponse.class)
-            .block();
+    try {
+      OLSOntologiesResponse response =
+          terminologyService
+              .get()
+              .uri(
+                  uriBuilder ->
+                      uriBuilder
+                          .path("/ontologies")
+                          .queryParam("page", 0)
+                          .queryParam("size", 1000)
+                          .build())
+              .retrieve()
+              .bodyToMono(OLSOntologiesResponse.class)
+              .block();
 
-    return Arrays.stream(Objects.requireNonNull(response).get_embedded().getOntologies())
-        .map(
-            ontology ->
-                new CodeSystem()
-                    .externalId(ontology.getOntologyId())
-                    .uri(ontology.getConfig().getId())
-                    .name(ontology.getConfig().getTitle())
-                    .shortName(ontology.getConfig().getPreferredPrefix()))
-        .collect(
-            Collectors.toMap(
-                CodeSystem::getUri, Function.identity(), (existing, replacement) -> existing));
+      return Arrays.stream(Objects.requireNonNull(response).get_embedded().getOntologies())
+          .map(
+              ontology ->
+                  new CodeSystem()
+                      .externalId(ontology.getOntologyId())
+                      .uri(ontology.getConfig().getId())
+                      .name(ontology.getConfig().getTitle())
+                      .shortName(ontology.getConfig().getPreferredPrefix()))
+          .collect(
+              Collectors.toMap(
+                  CodeSystem::getUri, Function.identity(), (existing, replacement) -> existing));
+    } catch (Exception e) {
+      LOGGER.severe(e.getMessage());
+    }
+    return new HashMap<>();
   }
 }
