94714001ec318b5d8154e944c722f46ad6a86a8e
Franz Matthies <fmatthies@users.noreply.github.com>
2023-07-04 11:23:21 +0200

    implemented elasticsearch testcontainers

--- src/test/java/care/smith/top/backend/service/nlp/AbstractNLPTest.java
+++ src/test/java/care/smith/top/backend/service/nlp/AbstractNLPTest.java
@@ -4,19 +4,36 @@ import care.smith.top.backend.repository.nlp.ConceptNodeRepository;
 import care.smith.top.backend.repository.nlp.DocumentNodeRepository;
 import care.smith.top.backend.repository.nlp.DocumentRepository;
 import care.smith.top.backend.repository.nlp.PhraseNodeRepository;
-import care.smith.top.backend.service.AbstractTest;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.neo4j.harness.Neo4j;
 import org.neo4j.harness.Neo4jBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
 
+import java.io.IOException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @Transactional(propagation = Propagation.NEVER)
-public abstract class AbstractNLPTest extends AbstractTest {
+public abstract class AbstractNLPTest {//extends AbstractTest {
 
     @Autowired
     ConceptService conceptService;
@@ -33,7 +50,17 @@ public abstract class AbstractNLPTest extends AbstractTest {
     @Autowired
     PhraseNodeRepository phraseRepository;
 
-    private static Neo4j embeddedNeo4j;
+    protected static Neo4j embeddedNeo4j;
+    protected static ElasticsearchClient esClient;
+    protected static final String[] ELASTIC_INDEX = new String[] {"test_documents"};
+    @Container
+    protected static final ElasticsearchContainer elasticsearchContainer = new DocumentElasticsearchContainer();
+
+    private static final Map<String, String> documents =
+            Map.of(
+                    "test01", "What do we have here? A test document. With an entity. Nice.",
+                    "test02", "Another document is here. It has two entities.",
+                    "test03", "And a third document; but this one features nothing");
 
     @BeforeAll
     static void initializeDBs() {
@@ -42,6 +69,7 @@ public abstract class AbstractNLPTest extends AbstractTest {
                 .build();
 
         // use elasticsearch test instance here
+        setUpESIndex();
     }
 
     @DynamicPropertySource
@@ -54,5 +82,49 @@ public abstract class AbstractNLPTest extends AbstractTest {
     @AfterAll
     static void stopDBs() {
         embeddedNeo4j.close();
+        esClient.shutdown();
+        elasticsearchContainer.stop();
+    }
+
+    protected static void setUpESIndex() {
+        elasticsearchContainer.start();
+        RestClient restClient =
+                RestClient.builder(HttpHost.create(elasticsearchContainer.getHttpHostAddress())).build();
+
+        ElasticsearchTransport transport =
+                new RestClientTransport(restClient, new JacksonJsonpMapper());
+
+        esClient = new ElasticsearchClient(transport);
+        assertNotNull(esClient);
+
+        try {
+            esClient.index(i -> i
+                    .id("01")
+                    .index(ELASTIC_INDEX[0])
+                    .document( new TextDocument("test01", documents.get("test01")))
+            );
+            esClient.index(i -> i
+                    .id("02")
+                    .index(ELASTIC_INDEX[0])
+                    .document( new TextDocument("test02", documents.get("test02")))
+            );
+            esClient.index(i -> i
+                    .id("03")
+                    .index(ELASTIC_INDEX[0])
+                    .document( new TextDocument("test03", documents.get("test03")))
+            );
+            await().until(() -> esClient.count().count() == 3);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static class TextDocument {
+        public String name;
+        public String text;
+        public TextDocument(String name, String text) {
+            this.name = name;
+            this.text = text;
+        }
     }
 }
--- src/test/java/care/smith/top/backend/service/nlp/DocumentServiceTest.java
+++ src/test/java/care/smith/top/backend/service/nlp/DocumentServiceTest.java
@@ -1,17 +1,10 @@
 package care.smith.top.backend.service.nlp;
 
-import care.smith.top.backend.model.nlp.DocumentEntity;
 import care.smith.top.model.Document;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
@@ -21,34 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
 @Testcontainers
 @ExtendWith(SpringExtension.class)
 class DocumentServiceTest extends AbstractNLPTest {
-    @Container
-    private static final ElasticsearchContainer elasticsearchContainer = new DocumentElasticsearchContainer();
-
-//    @Autowired
-//    private ElasticsearchTemplate template;
-
-    @BeforeAll
-    static void setUp() {
-        elasticsearchContainer.start();
-    }
-
-    @AfterAll
-    static void destroy() {
-        elasticsearchContainer.stop();
-    }
 
     @BeforeEach
     void testIsContainerRunning() {
         assertTrue(elasticsearchContainer.isRunning());
-//        recreateIndex();
     }
-
-    // TODO: provide this example data via elasticsearch testcontainers
-    // Since indexing from within Spring Apps did not work on creation time of Test Cases,
-    // there are these three documents prepared on a test index within the proper ES instance:
-    //      {"id": "01", "name": "test01", "text": "What do we have here? A test document. With an entity. Nice."},
-    //      {"id": "02", "name": "test02", "text": "Another document is here. It has two entities."},
-    //      {"id": "03", "name": "test03", "text": "And a third document; but this one features nothing."}
 
     @Test
     void getDocumentByName() {
@@ -100,11 +70,4 @@ class DocumentServiceTest extends AbstractNLPTest {
     @Test
     void getDocumentsForConcepts() {
     }
-
-//    private void recreateIndex() {
-//        if (template.indexOps(DocumentEntity.class).exists()) {
-//            template.indexOps(DocumentEntity.class).delete();
-//            template.indexOps(DocumentEntity.class).create();
-//        }
-//    }
 }
