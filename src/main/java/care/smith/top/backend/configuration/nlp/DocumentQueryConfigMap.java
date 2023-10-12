960998fbf5d1b03467d4efad3a04368b80e55e3d
Franz Matthies <fmatthies@users.noreply.github.com>
2023-10-12 14:44:23 +0200

    introduced final variables in DocumentQueryConfigMap

--- src/main/java/care/smith/top/backend/configuration/nlp/DocumentQueryConfigMap.java
+++ src/main/java/care/smith/top/backend/configuration/nlp/DocumentQueryConfigMap.java
@@ -5,6 +5,8 @@ import care.smith.top.top_document_query.adapter.config.GraphDBConfig;
 import care.smith.top.top_document_query.adapter.config.TextAdapterConfig;
 import org.springframework.core.env.Environment;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
@@ -12,32 +14,79 @@ import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class DocumentQueryConfigMap {
-  private final Logger LOGGER = Logger.getLogger(DocumentQueryConfigMap.class.getName());
+  private static final String DEFAULT_ES_URL = "http://localhost";
+  private static final String DEFAULT_ES_PORT = "9200";
+  private static final String DEFAULT_ES_INDEX = "documents";
+  private static final String DEFAULT_ES_FIELD = "text";
+  private static final String DEFAULT_NEO4J_URL = "bolt://localhost";
+  private static final String DEFAULT_NEO4J_PORT = "7687";
+  private static final String SPRING_ES_URI_PROP = "spring.elasticsearch.uris";
+  private static final String SPRING_ES_USERNAME_PROP = "spring.elasticsearch.username";
+  private static final String SPRING_ES_PASSWORD_PROP = "spring.elasticsearch.password";
+  private static final String SPRING_ES_INDEX_PROP = "spring.elasticsearch.index.name";
+  private static final String SPRING_NEO4J_URI_PROP = "spring.neo4j.uri";
+  private static final String SPRING_NEO4J_USERNAME_PROP = "spring.neo4j.authentication.username";
+  private static final String SPRING_NEO4J_PASSWORD_PROP = "spring.neo4j.authentication.password";
+  private static final String TOP_SECURITY_GRAPHDB_USERNAME = "top.documents.security.graphdb.username";
+  private static final String TOP_SECURITY_GRAPHDB_PASSWORD = "top.documents.security.graphdb.password";
+  private static final String TOP_SECURITY_DOCUMENTDB_USERNAME = "top.documents.security.documentdb.username";
+  private static final String TOP_SECURITY_DOCUMENTDB_PASSWORD = "top.documents.security.documentdb.password";
+  private static final String TOP_DATA_SOURCE_CONFIG_DIR_DOCUMENTS = "top.documents.data-source-config-dir";
+  private static final Logger LOGGER = Logger.getLogger(DocumentQueryConfigMap.class.getName());
   private Map<String, Object> configMap;
   private String name;
 
   public Map<String, Object> getConfigMap() {
     return configMap;
   }
-
   public void setConfigMap(Map<String, Object> configMap) {
     this.configMap = configMap;
   }
 
   public void setConfigMap(TextAdapterConfig config, Environment environment) {
-    this.configMap = new HashMap<>() {{
-      put("spring.elasticsearch.uris", config.getConnection().getUrl() + ":" + config.getConnection().getPort());
-      put("spring.elasticsearch.index.name", Arrays.stream(config.getIndex()).findFirst().orElse("documents")); //ToDo: look into this - provide list here possible, too?
-      put("spring.neo4j.uri", config.getGraphDB().getConnection().getUrl() + ":" + config.getGraphDB().getConnection().getPort());
-      put("spring.neo4j.authentication.username",
-          (config.getGraphDB().getAuthentication() == null || config.getGraphDB().getAuthentication().getUsername() == null) ?
-              environment.getProperty("top.documents.security.neo4j.username") : config.getGraphDB().getAuthentication().getUsername()
-      );
-      put("spring.neo4j.authentication.password",
-          (config.getGraphDB().getAuthentication() == null || config.getGraphDB().getAuthentication().getPassword() == null) ?
-              environment.getProperty("top.documents.security.neo4j.password") : config.getGraphDB().getAuthentication().getPassword()
-      );
-    }};
+    this.configMap =
+        new HashMap<>() {
+          {
+            if (config.getConnection() != null) {
+              put(SPRING_ES_URI_PROP, config.getConnection().getUrl() + ":" + config.getConnection().getPort());
+            } else {
+              put(SPRING_ES_URI_PROP, DEFAULT_ES_URL + ":" + DEFAULT_ES_PORT);
+            }
+            if (config.getIndex() != null) {
+              // ToDo: look into this - provide list here possible, too?
+              put(SPRING_ES_INDEX_PROP, Arrays.stream(config.getIndex()).findFirst().orElse(DEFAULT_ES_INDEX));
+            } else {
+              put(SPRING_ES_INDEX_PROP, DEFAULT_ES_INDEX);
+            }
+            if (config.getGraphDB() != null) {
+              if (config.getGraphDB().getConnection() != null) {
+                String url =
+                    config.getGraphDB().getConnection().getUrl() != null
+                        ? config.getGraphDB().getConnection().getUrl()
+                        : DEFAULT_NEO4J_URL;
+                String port =
+                    config.getGraphDB().getConnection().getPort() != null
+                        ? config.getGraphDB().getConnection().getPort()
+                        : DEFAULT_NEO4J_PORT;
+                put(SPRING_NEO4J_URI_PROP, url + ":" + port);
+              } else {
+                put(SPRING_NEO4J_URI_PROP, DEFAULT_NEO4J_URL + ":" + DEFAULT_NEO4J_PORT);
+              }
+              put(
+                  SPRING_NEO4J_USERNAME_PROP,
+                  (config.getGraphDB().getAuthentication() == null
+                          || config.getGraphDB().getAuthentication().getUsername() == null)
+                      ? environment.getProperty(TOP_SECURITY_GRAPHDB_USERNAME)
+                      : config.getGraphDB().getAuthentication().getUsername());
+              put(
+                  SPRING_NEO4J_PASSWORD_PROP,
+                  (config.getGraphDB().getAuthentication() == null
+                          || config.getGraphDB().getAuthentication().getPassword() == null)
+                      ? environment.getProperty(TOP_SECURITY_GRAPHDB_PASSWORD)
+                      : config.getGraphDB().getAuthentication().getPassword());
+            }
+          }
+        };
   }
 
   public String getName() {
@@ -49,8 +98,8 @@ public class DocumentQueryConfigMap {
   }
 
   public DocumentQueryConfigMap(Environment environment) {
-    TextAdapterConfig config = getFirstTextAdapterConfig(environment.getProperty("top.documents.data-source-config-dir"));
-    if (config == null) config = defaultConfig();
+    TextAdapterConfig config = getFirstTextAdapterConfig(environment.getProperty(TOP_DATA_SOURCE_CONFIG_DIR_DOCUMENTS));
+    if (config == null) config = defaultConfig(environment);
     this.setName(config.getId());
     this.setConfigMap(config, environment);
   }
@@ -84,21 +133,43 @@ public class DocumentQueryConfigMap {
     return null;
   }
 
-  private TextAdapterConfig defaultConfig() {
+  private TextAdapterConfig defaultConfig(Environment env) {
     TextAdapterConfig config = new TextAdapterConfig();
     config.setId("Default Adapter");
 
     Connection elastic_connection = new Connection();
-    elastic_connection.setUrl("http://localhost");
-    elastic_connection.setPort("9200");
 
+    elastic_connection.setUrl(DEFAULT_ES_URL);
+    elastic_connection.setPort(DEFAULT_ES_PORT);
+    if (env.getProperty(SPRING_ES_URI_PROP) != null) {
+      URL url;
+      try {
+        url = new URL(env.getProperty(SPRING_ES_URI_PROP));
+      } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      }
+      elastic_connection.setUrl(url.getHost());
+      elastic_connection.setPort(url.getPort() != -1 ? String.valueOf(url.getPort()) : DEFAULT_ES_PORT);
+    }
     config.setConnection(elastic_connection);
-    config.setIndex(new String[]{"documents"});
-    config.setField(new String[]{"text"});
+
+    config.setIndex(new String[]{ DEFAULT_ES_INDEX });
+    if (env.getProperty(SPRING_ES_INDEX_PROP) != null) config.setIndex(new String[]{ env.getProperty(SPRING_ES_INDEX_PROP) });
+    config.setField(new String[]{DEFAULT_ES_FIELD});
 
     Connection neo4j_connection = new Connection();
-    neo4j_connection.setUrl("bolt://localhost");
-    neo4j_connection.setPort("7687");
+    neo4j_connection.setUrl(DEFAULT_NEO4J_URL);
+    neo4j_connection.setPort(DEFAULT_NEO4J_PORT);
+    if (env.getProperty(SPRING_NEO4J_URI_PROP) != null) {
+      URL url;
+      try {
+        url = new URL(env.getProperty(SPRING_NEO4J_URI_PROP));
+      } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      }
+      neo4j_connection.setUrl(url.getHost());
+      neo4j_connection.setPort(url.getPort() != -1 ? String.valueOf(url.getPort()) : DEFAULT_NEO4J_PORT);
+    }
 
     GraphDBConfig neo4j = new GraphDBConfig();
     neo4j.setConnection(neo4j_connection);
