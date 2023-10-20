d7ebf6187fc27a9653fbee76ee8e85b09e5ec120
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2023-10-21 01:05:10 +0200

    Fix docker compose testcontainer compatibility issues

--- src/test/java/care/smith/top/backend/service/CodeServiceTest.java
+++ src/test/java/care/smith/top/backend/service/CodeServiceTest.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
 import java.util.Collections;
 import java.util.logging.Logger;
 
@@ -62,15 +61,14 @@ public class CodeServiceTest extends AbstractTest {
       FileUtils.copyInputStreamToFile(CodeServiceTest.class.getResourceAsStream("/ols4/test-ontology.json"), testOntologyConfigFile);
 
       olsContainer = new DockerComposeContainer(dockerComposeFile)
+              .withLocalCompose(true)
+              .withOptions("--compatibility")
               .withEnv("OLS4_CONFIG", testOntologyConfigFile.getAbsolutePath())
-              .withServices("ols4-dataload", "ols4-solr", "ols4-neo4j")
+              .withServices("ols4-dataload", "ols4-solr", "ols4-neo4j", "ols4-backend")
               .withExposedService("ols4-backend", 8080,
                       Wait.forHttp("/api")
                               .forStatusCode(200)
-                              .forStatusCode(401)
-                              .withStartupTimeout(Duration.ofMinutes(30)))
-              .withStartupTimeout(Duration.ofMinutes(30))
-      ;
+                              .forStatusCode(401));
 
       LOGGER.info("**** Starting OLS4 container...");
 
@@ -87,16 +85,6 @@ public class CodeServiceTest extends AbstractTest {
       codeService.setEndpoint(endpoint);
     }
   }
-
-//  @AfterAll
-//  @Timeout(value = 30, unit = TimeUnit.MINUTES)
-//  void stopOLS() {
-//    Instant finish = Instant.now();
-//    long timeElapsed = Duration.between(start, finish).toSeconds();
-//    LOGGER.info(String.format("**** Stopping OLS4 container after %d seconds...", timeElapsed);
-//    olsContainer.stop();
-//    LOGGER.info("**** OLS4 container stopped.");
-//  }
 
   @Test
   void getSuggestions() {
