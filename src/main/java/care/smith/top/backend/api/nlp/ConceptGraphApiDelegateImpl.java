f477fba2e50e0b65492ccb28cb4dc1ace9220238
Franz Matthies <fmatthies@users.noreply.github.com>
2023-10-21 17:08:04 +0200

    implemented process overview endpoint access

--- src/main/java/care/smith/top/backend/api/nlp/ConceptGraphApiDelegateImpl.java
+++ src/main/java/care/smith/top/backend/api/nlp/ConceptGraphApiDelegateImpl.java
@@ -1,14 +1,36 @@
 package care.smith.top.backend.api.nlp;
 
 import care.smith.top.backend.api.ConceptgraphsApiDelegate;
+import care.smith.top.backend.service.nlp.ConceptGraphsService;
+import care.smith.top.model.ConceptGraph;
+import care.smith.top.model.ConceptGraphProcess;
 import care.smith.top.model.ConceptGraphStat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ConceptGraphApiDelegateImpl implements ConceptgraphsApiDelegate {
+  private final ConceptGraphsService conceptGraphsService;
+
+  public ConceptGraphApiDelegateImpl(ConceptGraphsService conceptGraphsService) {
+    this.conceptGraphsService = conceptGraphsService;
+  }
+
   @Override
   public ResponseEntity<List<ConceptGraphStat>> getConceptGraphStatistics(List<String> include, String process) {
     return ConceptgraphsApiDelegate.super.getConceptGraphStatistics(include, process);
+  }
+
+  @Override
+  public ResponseEntity<ConceptGraph> getConceptGraph(List<String> include, String processId, String graphId) {
+    return ConceptgraphsApiDelegate.super.getConceptGraph(include, processId, graphId);
+  }
+
+  @Override
+  public ResponseEntity<List<ConceptGraphProcess>> getStoredProcesses(List<String> include) {
+    conceptGraphsService.getAllStoredProcesses();
+    return ConceptgraphsApiDelegate.super.getStoredProcesses(include);
   }
 }
