b113f0d68d604135ad1f38eb24bdcff37622075f
Franz Matthies <fmatthies@users.noreply.github.com>
2023-10-24 17:08:50 +0200

    added methods for starting a pipeline -> now with ...ApiDelegateImpl as well

--- src/main/java/care/smith/top/backend/api/nlp/ConceptGraphApiDelegateImpl.java
+++ src/main/java/care/smith/top/backend/api/nlp/ConceptGraphApiDelegateImpl.java
@@ -7,9 +7,14 @@ import care.smith.top.model.ConceptGraphProcess;
 import care.smith.top.model.ConceptGraphStat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Service
 public class ConceptGraphApiDelegateImpl implements ConceptgraphsApiDelegate {
@@ -33,5 +38,44 @@ public class ConceptGraphApiDelegateImpl implements ConceptgraphsApiDelegate {
   @Override
   public ResponseEntity<List<ConceptGraphProcess>> getStoredProcesses(List<String> include) {
     return ResponseEntity.ok(conceptGraphsService.getAllStoredProcesses());
+  }
+
+  @Override
+  public ResponseEntity<Map<String, ConceptGraphStat>> startConceptGraphPipeline (
+      String process, MultipartFile data,
+      List<String> include,
+      String lang, Boolean skipPresent,
+      MultipartFile labels,
+      MultipartFile dataConfig, MultipartFile embeddingConfig,
+      MultipartFile clusteringConfig, MultipartFile graphConfig
+  ) {
+    Map<String, File> configMap = Map.of(
+        "data", dataConfig,
+        "embedding", embeddingConfig,
+        "clustering", clusteringConfig,
+        "graph", graphConfig)
+        .entrySet()
+        .stream()
+        .filter(entry -> entry.getValue() != null)
+        .collect(Collectors.toMap(Map.Entry::getKey, e -> {
+              try {
+                return e.getValue().getResource().getFile();
+              } catch (IOException ex) {
+                throw new RuntimeException(ex);
+              }
+            }));
+
+    try {
+      return ResponseEntity.ok(
+          conceptGraphsService.initPipelineWithConfigs(
+              data.getResource().getFile(),
+              labels != null ? labels.getResource().getFile(): null,
+              process, lang,
+              skipPresent, configMap
+          )
+      );
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
