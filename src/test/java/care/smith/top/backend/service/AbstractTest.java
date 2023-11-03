c8f54239dfa5f359c02fd0b0d26679b2f4bae5c6
Christoph Beger <ChristophB@users.noreply.github.com>
2023-11-03 12:46:29 +0100

    Fix CodeServiceTest (#180)
    * Make ols mock server available to all test classes

--- src/test/java/care/smith/top/backend/service/AbstractTest.java
+++ src/test/java/care/smith/top/backend/service/AbstractTest.java
@@ -2,12 +2,22 @@ package care.smith.top.backend.service;
 
 import care.smith.top.backend.api.OrganisationApiDelegateImpl;
 import care.smith.top.backend.repository.jpa.*;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public abstract class AbstractTest {
+  static HttpServer olsServer;
   @Autowired OrganisationApiDelegateImpl organisationApiDelegate;
   @Autowired OrganisationService organisationService;
   @Autowired OrganisationRepository organisationRepository;
@@ -23,9 +33,44 @@ public abstract class AbstractTest {
   @Autowired UserService userService;
   @Autowired OrganisationMembershipRepository organisationMembershipRepository;
 
+  @BeforeAll
+  static void initializeOLS() throws IOException {
+    olsServer = HttpServer.create(new InetSocketAddress(9000), 0);
+    olsServer.createContext(
+        "/api/ontologies", new CodeServiceTest.OlsHttpHandler("/ols4_fixtures/ontologies.json"));
+    olsServer.createContext(
+        "/api/select", new CodeServiceTest.OlsHttpHandler("/ols4_fixtures/select.json"));
+    olsServer.start();
+  }
+
+  @AfterAll
+  static void stopOLS() {
+    olsServer.stop(0);
+  }
+
   @BeforeEach
   public void resetState() {
     organisationRepository.deleteAll();
     userRepository.deleteAll();
+  }
+
+  static class OlsHttpHandler implements HttpHandler {
+    private final String resourcePath;
+
+    public OlsHttpHandler(String resourcePath) {
+      this.resourcePath = resourcePath;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+      try (InputStream resource = CodeServiceTest.class.getResourceAsStream(resourcePath)) {
+        assert resource != null;
+        byte[] response = resource.readAllBytes();
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
+        exchange.getResponseBody().write(response);
+      }
+      exchange.close();
+    }
   }
 }
--- src/test/java/care/smith/top/backend/service/CodeServiceTest.java
+++ src/test/java/care/smith/top/backend/service/CodeServiceTest.java
@@ -4,51 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import care.smith.top.model.CodePage;
 import care.smith.top.model.CodeSystemPage;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
 import java.util.Collections;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-/**
- * @author ralph
- */
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CodeServiceTest extends AbstractTest {
-  @Autowired private MutableOLSCodeService codeService;
-  private HttpServer olsServer;
-
-  @BeforeAll
-  void initializeOLS() throws IOException {
-    InetSocketAddress address = new InetSocketAddress(9000);
-
-    olsServer = HttpServer.create(address, 0);
-    olsServer.createContext(
-        "/api/ontologies", new OlsHttpHandler("/ols4_fixtures/ontologies.json"));
-    olsServer.createContext("/api/select", new OlsHttpHandler("/ols4_fixtures/select.json"));
-    olsServer.start();
-
-    String endpoint = String.format("http://127.0.0.1:%d/api", address.getPort());
-    codeService.setEndpoint(endpoint);
-  }
-
-  @AfterAll
-  void stopOLS() {
-    olsServer.stop(0);
-  }
+  @Autowired OLSCodeService codeService;
 
   @Test
   void getSuggestions() {
@@ -60,25 +23,5 @@ public class CodeServiceTest extends AbstractTest {
   void getCodeSystems() {
     CodeSystemPage codeSystems = codeService.getCodeSystems(null, null, null, 1);
     assertThat(codeSystems).isNotNull().satisfies(cs -> assertThat(cs.getContent()).isNotEmpty());
-  }
-
-  static class OlsHttpHandler implements HttpHandler {
-    private final String resourcePath;
-
-    public OlsHttpHandler(String resourcePath) {
-      this.resourcePath = resourcePath;
-    }
-
-    @Override
-    public void handle(HttpExchange exchange) throws IOException {
-      try (InputStream resource = CodeServiceTest.class.getResourceAsStream(resourcePath)) {
-        assert resource != null;
-        byte[] response = resource.readAllBytes();
-        exchange.getResponseHeaders().set("Content-Type", "application/json");
-        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length);
-        exchange.getResponseBody().write(response);
-      }
-      exchange.close();
-    }
   }
 }
