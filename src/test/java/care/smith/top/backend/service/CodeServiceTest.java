96c17ae879318cfc8983dd71169d140a177d118d
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2023-12-15 10:32:45 +0100

    Re-enable OLS related tests
    Looks like they were accidentally disabled due to merging issues

--- src/test/java/care/smith/top/backend/service/CodeServiceTest.java
+++ src/test/java/care/smith/top/backend/service/CodeServiceTest.java
@@ -6,7 +6,6 @@ import care.smith.top.backend.AbstractTest;
 import care.smith.top.model.CodePage;
 import care.smith.top.model.CodeSystemPage;
 import java.util.Collections;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,14 +15,12 @@ public class CodeServiceTest extends AbstractTest {
   @Autowired OLSCodeService codeService;
 
   @Test
-  @Disabled
   void getSuggestions() {
     CodePage suggestions = codeService.getCodeSuggestions(null, "term", Collections.emptyList(), 1);
     assertThat(suggestions).isNotNull().satisfies(s -> assertThat(s.getContent()).isNotEmpty());
   }
 
   @Test
-  @Disabled
   void getCodeSystems() {
     CodeSystemPage codeSystems = codeService.getCodeSystems(null, null, null, 1);
     assertThat(codeSystems).isNotNull().satisfies(cs -> assertThat(cs.getContent()).isNotEmpty());
