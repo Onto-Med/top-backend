ee1748a92d969516f441c6581621844a69022da4
Franz Matthies <fmatthies@users.noreply.github.com>
2024-01-15 15:03:01 +0100

    only conceptById test; the other is not feasible

--- src/test/java/care/smith/top/backend/service/nlp/AbstractNLPTest.java
+++ src/test/java/care/smith/top/backend/service/nlp/AbstractNLPTest.java
@@ -20,7 +20,6 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
@@ -34,7 +33,6 @@ import org.neo4j.harness.Neo4jBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.neo4j.core.Neo4jClient;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Propagation;
@@ -62,20 +60,17 @@ public abstract class AbstractNLPTest extends AbstractTest {
   private static final Map<String, Map<String, String>> documentNodes =
       Map.of(
           "d1", Map.of("docId", "d1", "name", "Document 1"),
-          "d2", Map.of("docId", "d2", "name", "Document 2")
-      );
+          "d2", Map.of("docId", "d2", "name", "Document 2"));
 
   private static final Map<String, Map<String, String>> conceptNodes =
       Map.of(
-          "c1", Map.of("conceptId", "c1", "labels", "phrase,here,another"),
-          "c2", Map.of("conceptId", "c2", "labels", "something,good")
-      );
+          "c1", Map.of("conceptId", "c1", "labels", "['phrase','here','another']"),
+          "c2", Map.of("conceptId", "c2", "labels", "['something','good']"));
 
   private static final Map<String, Map<String, String>> phraseNodes =
       Map.of(
           "p1", Map.of("phraseId", "p1", "phrase", "one phrase here", "exemplar", "true"),
-          "p2", Map.of("phraseId", "p2", "phrase", "another phrase there", "exemplar", "false")
-      );
+          "p2", Map.of("phraseId", "p2", "phrase", "another phrase there", "exemplar", "false"));
 
   private static final String HAS_PHRASE_REL = "HAS_PHRASE";
   private static final String IN_CONCEPT_REL = "IN_CONCEPT";
@@ -86,8 +81,7 @@ public abstract class AbstractNLPTest extends AbstractTest {
           "d1", List.of(Pair.of(HAS_PHRASE_REL, "p2")),
           "d2", List.of(Pair.of(HAS_PHRASE_REL, "p1"), Pair.of(HAS_PHRASE_REL, "p2")),
           "p1", List.of(Pair.of(IN_CONCEPT_REL, "c1"), Pair.of(NEIGHBOR_OF_REL, "p2")),
-          "p2", List.of(Pair.of(IN_CONCEPT_REL, "c2"), Pair.of(NEIGHBOR_OF_REL, "p1"))
-      );
+          "p2", List.of(Pair.of(IN_CONCEPT_REL, "c2"), Pair.of(NEIGHBOR_OF_REL, "p1")));
 
   protected static Neo4j embeddedNeo4j;
   protected static Session neo4jSession;
@@ -126,29 +120,44 @@ public abstract class AbstractNLPTest extends AbstractTest {
 
   protected static void setUpNeo4jDB() {
     try (Driver driver = GraphDatabase.driver(embeddedNeo4j.boltURI());
-         Session session = driver.session()
-    ) {
-      Map<String,String> typeMap = Map.of("d", "Document", "c", "Concept", "p", "Phrase");
-      Map<String,String> idMap = Map.of("d", "documentId", "c", "conceptId", "p", "phraseId");
+        Session session = driver.session()) {
+      Map<String, String> typeMap = Map.of("d", "Document", "c", "Concept", "p", "Phrase");
+      Map<String, String> idMap = Map.of("d", "documentId", "c", "conceptId", "p", "phraseId");
       neo4jSession = session;
-      documentNodes.forEach((key, value) -> neo4jSession.run(
-          String.format("CREATE (:Document {docId: '%s', name: '%s'})", value.get("docId"), value.get("name"))));
-      conceptNodes.forEach((key, value) -> neo4jSession.run(
-          String.format("CREATE (:Concept {conceptId: '%s', labels: '%s'})", value.get("conceptId"), value.get("labels"))));
-      phraseNodes.forEach((key, value) -> neo4jSession.run(
-          String.format("CREATE (:Phrase {phraseId: '%s', phrase: '%s', exemplar: '%s'})", value.get("phraseId"), value.get("phrase"), value.get("exemplar"))));
-      relations.forEach((key, list) -> {
-        String sId = idMap.get(key.substring(0,1));
-        String sType = typeMap.get(key.substring(0,1));
-        list.forEach(pair -> {
-          String tId = idMap.get(pair.getRight().substring(0,1));
-          String tType = typeMap.get(pair.getRight().substring(0,1));
-          String rType = pair.getLeft();
-          String query = String.format("MATCH (s:%s), (t:%s) WHERE s.%s = '%s' AND t.%s = '%s' CREATE (s)-[:%s]->(t)",
-              sType, tType, sId, key, tId, pair.getLeft(), rType );
-          neo4jSession.run(query);
-        });
-      });
+      documentNodes.forEach(
+          (key, value) ->
+              neo4jSession.run(
+                  String.format(
+                      "CREATE (:Document {docId: '%s', name: '%s'})",
+                      value.get("docId"), value.get("name"))));
+      conceptNodes.forEach(
+          (key, value) ->
+              neo4jSession.run(
+                  String.format(
+                      "CREATE (:Concept {conceptId: '%s', labels: %s})",
+                      value.get("conceptId"), value.get("labels"))));
+      phraseNodes.forEach(
+          (key, value) ->
+              neo4jSession.run(
+                  String.format(
+                      "CREATE (:Phrase {phraseId: '%s', phrase: '%s', exemplar: %s})",
+                      value.get("phraseId"), value.get("phrase"), value.get("exemplar"))));
+      relations.forEach(
+          (key, list) -> {
+            String sId = idMap.get(key.substring(0, 1));
+            String sType = typeMap.get(key.substring(0, 1));
+            list.forEach(
+                pair -> {
+                  String tId = idMap.get(pair.getRight().substring(0, 1));
+                  String tType = typeMap.get(pair.getRight().substring(0, 1));
+                  String rType = pair.getLeft();
+                  String query =
+                      String.format(
+                          "MATCH (s:%s), (t:%s) WHERE s.%s = '%s' AND t.%s = '%s' CREATE (s)-[:%s]->(t)",
+                          sType, tType, sId, key, tId, pair.getLeft(), rType);
+                  neo4jSession.run(query);
+                });
+          });
     }
   }
 
--- src/test/java/care/smith/top/backend/service/nlp/ConceptClusterServiceTest.java
+++ src/test/java/care/smith/top/backend/service/nlp/ConceptClusterServiceTest.java
@@ -2,13 +2,20 @@ package care.smith.top.backend.service.nlp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import care.smith.top.model.ConceptCluster;
 import org.junit.jupiter.api.Test;
 
 class ConceptClusterServiceTest extends AbstractNLPTest {
 
   @Test
-  void conceptById() {}
+  void conceptById() {
+    // see AbstractNLPTest for creation of the Neo4J DB entries
+    ConceptCluster cluster1 = conceptService.conceptById("c1");
+    assertEquals("c1", cluster1.getId());
+    assertEquals("phrase, here, another", cluster1.getLabels());
 
-  @Test
-  void createSpecificGraphsInNeo4j() {}
+    ConceptCluster cluster2 = conceptService.conceptById("c2");
+    assertEquals("c2", cluster2.getId());
+    assertEquals("something, good", cluster2.getLabels());
+  }
 }
