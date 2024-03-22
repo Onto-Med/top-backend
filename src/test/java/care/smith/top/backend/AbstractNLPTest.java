43ea2ed4daea0b4614bb0024d9b9855b63f081d9
Christoph Beger <ChristophB@users.noreply.github.com>
2024-03-22 10:26:45 +0100

    Migrate embedded neo4j server to test extension

--- src/test/java/care/smith/top/backend/AbstractNLPTest.java
+++ src/test/java/care/smith/top/backend/AbstractNLPTest.java
@@ -1,6 +1,12 @@
 package care.smith.top.backend;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import care.smith.top.backend.configuration.TopBackendContextInitializer;
+import care.smith.top.backend.extension.Neo4JExtension;
 import care.smith.top.backend.model.neo4j.DocumentNodeEntity;
 import care.smith.top.backend.repository.neo4j.ConceptClusterNodeRepository;
 import care.smith.top.backend.repository.neo4j.DocumentNodeRepository;
@@ -16,148 +22,74 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.*;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.Session;
-import org.neo4j.harness.Neo4j;
-import org.neo4j.harness.Neo4jBuilders;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ContextConfiguration(initializers = TopBackendContextInitializer.class)
 public abstract class AbstractNLPTest {
   protected static Set<Document> documents1 = Set.of(new Document().id("d1").name("Document 1"));
   protected static Set<Document> documents2 = Set.of(new Document().id("d2").name("Document 2"));
-  protected static Set<Document> documents1_2 = Set.of(documents1.iterator().next(), documents2.iterator().next());
-
-  protected static List<ConceptCluster> concepts1 = List.of(new ConceptCluster().id("c1").labels("phrase, here, another"));
-  protected static List<ConceptCluster> concepts2 = List.of(new ConceptCluster().id("c2").labels("something, good"));
+  protected static Set<Document> documents1_2 =
+      Set.of(documents1.iterator().next(), documents2.iterator().next());
+  protected static List<ConceptCluster> concepts1 =
+      List.of(new ConceptCluster().id("c1").labels("phrase, here, another"));
+  protected static List<ConceptCluster> concepts2 =
+      List.of(new ConceptCluster().id("c2").labels("something, good"));
   protected static List<ConceptCluster> concepts1_2 = List.of(concepts1.get(0), concepts2.get(0));
-
-  protected static List<Phrase> phrases1 = List.of(new Phrase().id("p1").text("one phrase here").exemplar(true));
-  protected static List<Phrase> phrases2 = List.of(new Phrase().id("p2").text("another phrase there").exemplar(false));
+  protected static List<Phrase> phrases1 =
+      List.of(new Phrase().id("p1").text("one phrase here").exemplar(true));
+  protected static List<Phrase> phrases2 =
+      List.of(new Phrase().id("p2").text("another phrase there").exemplar(false));
   protected static List<Phrase> phrases1_2 = List.of(phrases1.get(0), phrases2.get(0));
-
-  private static final String HAS_PHRASE_REL = "HAS_PHRASE";
-  private static final String IN_CONCEPT_REL = "IN_CONCEPT";
-  private static final String NEIGHBOR_OF_REL = "NEIGHBOR_OF";
-
-  private static final Map<String, List<Pair<String, String>>> relations =
-      Map.of(
-          "d1", List.of(Pair.of(HAS_PHRASE_REL, "p2")),
-          "d2", List.of(Pair.of(HAS_PHRASE_REL, "p1"), Pair.of(HAS_PHRASE_REL, "p2")),
-          "p1", List.of(Pair.of(IN_CONCEPT_REL, "c1"), Pair.of(NEIGHBOR_OF_REL, "p2")),
-          "p2", List.of(Pair.of(IN_CONCEPT_REL, "c2"), Pair.of(NEIGHBOR_OF_REL, "p1")));
-
-  protected static Neo4j embeddedNeo4j;
   protected static HttpServer conceptGraphsApiService;
-  protected static Session neo4jSession;
-
+  @RegisterExtension static Neo4JExtension neo4JExtension = new Neo4JExtension();
   @Autowired protected ConceptClusterService conceptClusterService;
   @Autowired protected ConceptClusterNodeRepository conceptClusterNodeRepository;
   @Autowired protected PhraseNodeRepository phraseRepository;
   @Autowired protected DocumentNodeRepository documentNodeRepository;
 
-  @BeforeAll
-  static void setup() throws IOException, InstantiationException {
-    setUpNeo4jDB();
+  @DynamicPropertySource
+  static void dbProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.neo4j.uri", neo4JExtension.getEmbeddedNeo4j()::boltURI);
+    registry.add("spring.neo4j.authentication.username", () -> "neo4j");
+    registry.add("spring.neo4j.authentication.password", () -> null);
+  }
 
+  @BeforeAll
+  static void setup() throws IOException {
     conceptGraphsApiService = HttpServer.create(new InetSocketAddress(9007), 0);
     conceptGraphsApiService.createContext(
-        "/processes",
-        new ResourceHttpHandler("/concept_graphs_api_fixtures/get_processes.json"));
+        "/processes", new ResourceHttpHandler("/concept_graphs_api_fixtures/get_processes.json"));
     conceptGraphsApiService.createContext(
         "/graph/statistics",
         new ResourceHttpHandler("/concept_graphs_api_fixtures/get_statistics.json"));
     conceptGraphsApiService.createContext(
-        "/graph/0",
-        new ResourceHttpHandler("/concept_graphs_api_fixtures/get_concept_graph.json"));
+        "/graph/0", new ResourceHttpHandler("/concept_graphs_api_fixtures/get_concept_graph.json"));
     conceptGraphsApiService.start();
   }
 
-//  @DynamicPropertySource
-//  static void dbProperties(DynamicPropertyRegistry registry) {
-//    registry.add("spring.neo4j.uri", embeddedNeo4j::boltURI);
-//    registry.add("spring.neo4j.authentication.username", () -> "neo4j");
-//    registry.add("spring.neo4j.authentication.password", () -> null);
-//  }
-
   @AfterAll
   static void cleanup() {
-    embeddedNeo4j.close();
     conceptGraphsApiService.stop(0);
   }
 
-  protected static void setUpNeo4jDB() {
-    embeddedNeo4j = Neo4jBuilders.newInProcessBuilder().withDisabledServer().build();
-    try (Driver driver = GraphDatabase.driver(embeddedNeo4j.boltURI());
-        Session session = driver.session()) {
-      Map<String, String> typeMap = Map.of("d", "Document", "c", "Concept", "p", "Phrase");
-      Map<String, String> idMap = Map.of("d", "docId", "c", "conceptId", "p", "phraseId");
-      neo4jSession = session;
-      documents1_2.forEach(
-          document ->
-              neo4jSession.run(
-                  String.format(
-                      "CREATE (:Document {docId: '%s', name: '%s'})",
-                      document.getId(), document.getName())));
-      concepts1_2.forEach(
-          concept -> {
-              String labels = Arrays.stream(concept.getLabels().split(","))
-                  .map(s -> String.format("'%s'", s.trim())).collect(Collectors.joining(","));
-              neo4jSession.run(
-                  String.format(
-                      "CREATE (:Concept {conceptId: '%s', labels: %s})",
-                      concept.getId(), String.format("[%s]", labels)));
-          });
-      phrases1_2.forEach(
-          phrase ->
-              neo4jSession.run(
-                  String.format(
-                      "CREATE (:Phrase {phraseId: '%s', phrase: '%s', exemplar: %s})",
-                      phrase.getId(), phrase.getText(), phrase.isExemplar())));
-
-      relations.forEach(
-          (key, list) -> {
-            String sId = idMap.get(key.substring(0, 1));
-            String sType = typeMap.get(key.substring(0, 1));
-            list.forEach(
-                pair -> {
-                  String tId = idMap.get(pair.getRight().substring(0, 1));
-                  String tType = typeMap.get(pair.getRight().substring(0, 1));
-                  String rType = pair.getLeft();
-                  String query =
-                      String.format(
-                          "MATCH (s:%s), (t:%s) WHERE s.%s = '%s' AND t.%s = '%s' CREATE (s)-[:%s]->(t)",
-                          sType, tType, sId, key, tId, pair.getRight(), rType);
-                  neo4jSession.run(query);
-                });
-          });
-    }
-  }
-
-  protected static DocumentService mockedDocumentService() throws IOException, InstantiationException {
+  protected static DocumentService mockedDocumentService()
+      throws IOException, InstantiationException {
     PageImpl<Document> page1 = new PageImpl<>(List.of(new Document().id("d1").name("Document 1")));
     PageImpl<Document> page2 = new PageImpl<>(List.of(new Document().id("d2").name("Document 2")));
-    PageImpl<Document> page1_2 = new PageImpl<>(
-        List.of(new Document().id("d1").name("Document 1"), new Document().id("d2").name("Document 2")));
+    PageImpl<Document> page1_2 =
+        new PageImpl<>(
+            List.of(
+                new Document().id("d1").name("Document 1"),
+                new Document().id("d2").name("Document 2")));
     DocumentNodeEntity d1 = new DocumentNodeEntity("d1", "Document 1", Set.of());
     DocumentNodeEntity d2 = new DocumentNodeEntity("d2", "Document 2", Set.of());
 
@@ -188,14 +120,13 @@ public abstract class AbstractNLPTest {
 
     DocumentService documentService = mock(DocumentService.class);
     when(documentService.getDocumentNodeRepository()).thenReturn(documentNodeRepository);
-    when(documentService.getAdapterFromQuery(anyString(), anyString(), any()))
-        .thenReturn(adapter);
-    when(documentService.getAdapterForDataSource(anyString()))
-        .thenReturn(adapter);
+    when(documentService.getAdapterFromQuery(anyString(), anyString(), any())).thenReturn(adapter);
+    when(documentService.getAdapterForDataSource(anyString())).thenReturn(adapter);
     when(documentService.getDocumentIdsForQuery(anyString(), anyString(), any()))
         .thenReturn(List.of("d1", "d2"));
     when(documentService.getDocumentsForConceptIds(anySet(), anyBoolean())).thenCallRealMethod();
-    when(documentService.getDocumentsForConceptIds(anySet(), anyBoolean(), any())).thenCallRealMethod();
+    when(documentService.getDocumentsForConceptIds(anySet(), anyBoolean(), any()))
+        .thenCallRealMethod();
     when(documentService.getDocumentsForPhraseIds(anySet(), anyBoolean())).thenCallRealMethod();
     when(documentService.getDocumentsForPhraseTexts(anySet(), anyBoolean())).thenCallRealMethod();
 
---
+++ src/test/java/care/smith/top/backend/extension/Neo4JExtension.java
@@ -0,0 +1,118 @@
+package care.smith.top.backend.extension;
+
+import care.smith.top.model.ConceptCluster;
+import care.smith.top.model.Document;
+import care.smith.top.model.Phrase;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
+
+public class Neo4JExtension implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+  private static final String HAS_PHRASE_REL = "HAS_PHRASE";
+  private static final String IN_CONCEPT_REL = "IN_CONCEPT";
+  private static final String NEIGHBOR_OF_REL = "NEIGHBOR_OF";
+  private static final Map<String, List<Pair<String, String>>> relations =
+      Map.of(
+          "d1", List.of(Pair.of(HAS_PHRASE_REL, "p2")),
+          "d2", List.of(Pair.of(HAS_PHRASE_REL, "p1"), Pair.of(HAS_PHRASE_REL, "p2")),
+          "p1", List.of(Pair.of(IN_CONCEPT_REL, "c1"), Pair.of(NEIGHBOR_OF_REL, "p2")),
+          "p2", List.of(Pair.of(IN_CONCEPT_REL, "c2"), Pair.of(NEIGHBOR_OF_REL, "p1")));
+  protected static Set<Document> documents1 = Set.of(new Document().id("d1").name("Document 1"));
+  protected static Set<Document> documents2 = Set.of(new Document().id("d2").name("Document 2"));
+  protected static Set<Document> documents1_2 =
+      Set.of(documents1.iterator().next(), documents2.iterator().next());
+  protected static List<ConceptCluster> concepts1 =
+      List.of(new ConceptCluster().id("c1").labels("phrase, here, another"));
+  protected static List<ConceptCluster> concepts2 =
+      List.of(new ConceptCluster().id("c2").labels("something, good"));
+  protected static List<ConceptCluster> concepts1_2 = List.of(concepts1.get(0), concepts2.get(0));
+  protected static List<Phrase> phrases1 =
+      List.of(new Phrase().id("p1").text("one phrase here").exemplar(true));
+  protected static List<Phrase> phrases2 =
+      List.of(new Phrase().id("p2").text("another phrase there").exemplar(false));
+  protected static List<Phrase> phrases1_2 = List.of(phrases1.get(0), phrases2.get(0));
+  private static boolean started = false;
+  private static Neo4j embeddedNeo4j;
+  private static Session neo4jSession;
+
+  private static void setUpNeo4jDB() {
+    embeddedNeo4j = Neo4jBuilders.newInProcessBuilder().withDisabledServer().build();
+    try (Driver driver = GraphDatabase.driver(embeddedNeo4j.boltURI());
+        Session session = driver.session()) {
+      Map<String, String> typeMap = Map.of("d", "Document", "c", "Concept", "p", "Phrase");
+      Map<String, String> idMap = Map.of("d", "docId", "c", "conceptId", "p", "phraseId");
+      neo4jSession = session;
+      documents1_2.forEach(
+          document ->
+              neo4jSession.run(
+                  String.format(
+                      "CREATE (:Document {docId: '%s', name: '%s'})",
+                      document.getId(), document.getName())));
+      concepts1_2.forEach(
+          concept -> {
+            String labels =
+                Arrays.stream(concept.getLabels().split(","))
+                    .map(s -> String.format("'%s'", s.trim()))
+                    .collect(Collectors.joining(","));
+            neo4jSession.run(
+                String.format(
+                    "CREATE (:Concept {conceptId: '%s', labels: %s})",
+                    concept.getId(), String.format("[%s]", labels)));
+          });
+      phrases1_2.forEach(
+          phrase ->
+              neo4jSession.run(
+                  String.format(
+                      "CREATE (:Phrase {phraseId: '%s', phrase: '%s', exemplar: %s})",
+                      phrase.getId(), phrase.getText(), phrase.isExemplar())));
+
+      relations.forEach(
+          (key, list) -> {
+            String sId = idMap.get(key.substring(0, 1));
+            String sType = typeMap.get(key.substring(0, 1));
+            list.forEach(
+                pair -> {
+                  String tId = idMap.get(pair.getRight().substring(0, 1));
+                  String tType = typeMap.get(pair.getRight().substring(0, 1));
+                  String rType = pair.getLeft();
+                  String query =
+                      String.format(
+                          "MATCH (s:%s), (t:%s) WHERE s.%s = '%s' AND t.%s = '%s' CREATE (s)-[:%s]->(t)",
+                          sType, tType, sId, key, tId, pair.getRight(), rType);
+                  neo4jSession.run(query);
+                });
+          });
+    }
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    if (!started) {
+      started = true;
+      setUpNeo4jDB();
+      context
+          .getRoot()
+          .getStore(ExtensionContext.Namespace.GLOBAL)
+          .put("test-data-setup-started", this);
+    }
+  }
+
+  @Override
+  public void close() {
+    embeddedNeo4j.close();
+  }
+
+  public Neo4j getEmbeddedNeo4j() {
+    return embeddedNeo4j;
+  }
+}
