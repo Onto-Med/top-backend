83d04c41601d1894688bd1afc6ec49b47e68b4b4
Christoph Beger <ChristophB@users.noreply.github.com>
2024-11-29 09:31:56 +0100

    Add data source repositories

---
+++ src/main/java/care/smith/top/backend/repository/jpa/datasource/SubjectRepository.java
@@ -0,0 +1,8 @@
+package care.smith.top.backend.repository.jpa.datasource;
+
+import care.smith.top.backend.model.jpa.datasource.SubjectDao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<SubjectDao, SubjectDao.SubjectKey> {}
