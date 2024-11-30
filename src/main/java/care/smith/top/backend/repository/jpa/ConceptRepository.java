8ca96a8028039aa4d2fc0b1bda36cd729aadf179
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2024-11-30 12:13:12 +0100

    added method to populate SubConcepts as well for a chosen concept list

--- src/main/java/care/smith/top/backend/repository/jpa/ConceptRepository.java
+++ src/main/java/care/smith/top/backend/repository/jpa/ConceptRepository.java
@@ -1,12 +1,32 @@
 package care.smith.top.backend.repository.jpa;
 
+import care.smith.top.backend.model.jpa.EntityDao;
+import care.smith.top.backend.util.ApiModelMapper;
+import care.smith.top.model.Concept;
+import care.smith.top.model.Entity;
 import care.smith.top.model.EntityType;
+import care.smith.top.model.SingleConcept;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Repository
 public interface ConceptRepository extends EntityRepository {
   default long count() {
     return countByEntityTypeIn(
         new EntityType[] {EntityType.SINGLE_CONCEPT, EntityType.COMPOSITE_CONCEPT});
+  }
+
+  default void populateSubconcepts(List<Entity> concepts, String repositoryId) {
+    for (Entity concept : concepts) {
+      if (ApiModelMapper.isSingleConcept(concept)) {
+        EntityDao entityDao = findByIdAndRepositoryId(concept.getId(), repositoryId).orElseThrow();
+        Set<Entity> subConcepts = entityDao.getSubEntities().stream().map(EntityDao::toApiModel).collect(Collectors.toSet());
+        ((SingleConcept) concept).setSubConcepts(subConcepts.stream().map(sc -> (Concept) sc).collect(Collectors.toList()));
+      }
+    }
   }
 }
