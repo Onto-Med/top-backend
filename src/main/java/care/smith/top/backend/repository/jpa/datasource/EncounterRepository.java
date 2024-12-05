2463b09cb65da7c66847657ae434666e6f73f626
auciteli <auciteli@imise.uni-leipzig.de>
2024-12-05 15:47:47 +0100

    Add EncounterCSVImport

--- src/main/java/care/smith/top/backend/repository/jpa/datasource/EncounterRepository.java
+++ src/main/java/care/smith/top/backend/repository/jpa/datasource/EncounterRepository.java
@@ -1,9 +1,12 @@
 package care.smith.top.backend.repository.jpa.datasource;
 
 import care.smith.top.backend.model.jpa.datasource.EncounterDao;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EncounterRepository
-    extends JpaRepository<EncounterDao, EncounterDao.EncounterKey> {}
+    extends JpaRepository<EncounterDao, EncounterDao.EncounterKey> {
+  Optional<EncounterDao> findByDataSourceIdAndEncounterId(String dataSourceId, String encounterId);
+}
