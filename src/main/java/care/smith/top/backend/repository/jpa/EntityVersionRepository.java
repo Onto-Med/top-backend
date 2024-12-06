2384d3d2832adac8b4dad04dba319be03708dc90
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2024-12-06 13:02:12 +0100

    Fix identifier type of EntityVersionRepository

--- src/main/java/care/smith/top/backend/repository/jpa/EntityVersionRepository.java
+++ src/main/java/care/smith/top/backend/repository/jpa/EntityVersionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EntityVersionRepository extends JpaRepository<EntityVersionDao, String> {
+public interface EntityVersionRepository extends JpaRepository<EntityVersionDao, Long> {
   List<EntityVersionDao> findAllByEntity_RepositoryIdAndEntityIdOrderByVersionDesc(
       String repositoryId, String entityId);
 
