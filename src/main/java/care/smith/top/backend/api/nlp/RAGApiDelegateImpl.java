a13488a4da42716ba422e8d60b14ba1124f2d332
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2025-09-17 17:30:43 +0200

    forgot to add "Service" to RAGApiDelegateImpl

--- src/main/java/care/smith/top/backend/api/nlp/RAGApiDelegateImpl.java
+++ src/main/java/care/smith/top/backend/api/nlp/RAGApiDelegateImpl.java
@@ -5,37 +5,38 @@ import care.smith.top.backend.service.nlp.DocumentService;
 import care.smith.top.backend.service.nlp.RAGService;
 import care.smith.top.model.RAGAnswer;
 import care.smith.top.top_document_query.adapter.TextAdapter;
+import java.util.logging.Logger;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
-
+@Service
 public class RAGApiDelegateImpl implements RagApiDelegate {
-    private final Logger LOGGER = Logger.getLogger(RAGApiDelegateImpl.class.getName());
-    private final RAGService ragService;
-    private final DocumentService documentService;
+  private final Logger LOGGER = Logger.getLogger(RAGApiDelegateImpl.class.getName());
+  private final RAGService ragService;
+  private final DocumentService documentService;
 
-    public RAGApiDelegateImpl(RAGService ragService, DocumentService documentService) {
-        this.ragService = ragService;
-        this.documentService = documentService;
-    }
+  public RAGApiDelegateImpl(RAGService ragService, DocumentService documentService) {
+    this.ragService = ragService;
+    this.documentService = documentService;
+  }
 
-    @Override
-    public ResponseEntity<String> initializeRAG(String process, Boolean force, Object body) {
-        return ResponseEntity.ok(ragService.initializeRAG(process, force, (JSONObject) body));
-    }
+  @Override
+  public ResponseEntity<String> initializeRAG(String process, Boolean force, Object body) {
+    return ResponseEntity.ok(ragService.initializeRAG(process, force, (JSONObject) body));
+  }
 
-    @Override
-    public ResponseEntity<RAGAnswer> poseQuestionToRAG(String process, String q) {
-        return ResponseEntity.ok(ragService.poseQuestion(process, q));
-    }
+  @Override
+  public ResponseEntity<RAGAnswer> poseQuestionToRAG(String process, String q) {
+    return ResponseEntity.ok(ragService.poseQuestion(process, q));
+  }
 
-    private TextAdapter getTextAdapter(String dataSource) {
-        try {
-            return documentService.getAdapterForDataSource(dataSource);
-        } catch (InstantiationException e) {
-            LOGGER.severe("The text adapter '" + dataSource + "' could not be initialized.");
-            return null;
-        }
+  private TextAdapter getTextAdapter(String dataSource) {
+    try {
+      return documentService.getAdapterForDataSource(dataSource);
+    } catch (InstantiationException e) {
+      LOGGER.severe("The text adapter '" + dataSource + "' could not be initialized.");
+      return null;
     }
+  }
 }
