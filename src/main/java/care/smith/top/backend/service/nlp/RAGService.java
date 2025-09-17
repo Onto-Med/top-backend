3aba0e3d08312c3649cec429756bd74a6492282a
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2025-09-17 17:30:43 +0200

    forgot to add "Service" to RAGApiDelegateImpl

--- src/main/java/care/smith/top/backend/service/nlp/RAGService.java
+++ src/main/java/care/smith/top/backend/service/nlp/RAGService.java
@@ -1,37 +1,35 @@
 package care.smith.top.backend.service.nlp;
 
 import care.smith.top.model.RAGAnswer;
-import care.smith.top.top_document_query.concept_graphs_api.ConceptPipelineManager;
 import care.smith.top.top_document_query.concept_graphs_api.RAGManager;
+import java.net.MalformedURLException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.net.MalformedURLException;
-
 @Service
 public class RAGService {
-    private final RAGManager ragManager;
+  private final RAGManager ragManager;
 
-    public RAGService(@Value("${top.documents.concept-graphs-api.uri}") String conceptGraphsApiUri) {
-        RAGManager tmpManager;
-        try {
-            tmpManager = new RAGManager(conceptGraphsApiUri);
-        } catch (MalformedURLException e) {
-            try {
-                tmpManager = new RAGManager("http://localhost:9010");
-            } catch (MalformedURLException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-        ragManager = tmpManager;
+  public RAGService(@Value("${top.documents.concept-graphs-api.uri}") String conceptGraphsApiUri) {
+    RAGManager tmpManager;
+    try {
+      tmpManager = new RAGManager(conceptGraphsApiUri);
+    } catch (MalformedURLException e) {
+      try {
+        tmpManager = new RAGManager("http://localhost:9010");
+      } catch (MalformedURLException ex) {
+        throw new RuntimeException(ex);
+      }
     }
+    ragManager = tmpManager;
+  }
 
-    public String initializeRAG(String process, boolean force, JSONObject jsonBody) {
-        return ragManager.initRag(process, force, jsonBody);
-    }
+  public String initializeRAG(String process, boolean force, JSONObject jsonBody) {
+    return ragManager.initRag(process, force, jsonBody);
+  }
 
-    public RAGAnswer poseQuestion(String process, String question) {
-        return ragManager.poseQuestion(process, question);
-    }
+  public RAGAnswer poseQuestion(String process, String question) {
+    return ragManager.poseQuestion(process, question);
+  }
 }
