ff3f53527ed8e6937aeacb6601f4b5b8b8af5a8c
Christoph Beger <ChristophB@users.noreply.github.com>
2025-10-17 15:01:52 +0200

    Improve recursive sql query

--- src/main/java/care/smith/top/backend/repository/jpa/CodeRepository.java
+++ src/main/java/care/smith/top/backend/repository/jpa/CodeRepository.java
@@ -13,17 +13,17 @@ public interface CodeRepository
       nativeQuery = true,
       value =
           "WITH RECURSIVE code_tree AS ( "
-              + "SELECT id root_id, 0 level, * "
-              + "FROM code "
-              + "UNION ALL "
-              + "SELECT root_id, level + 1 level, c.* "
-              + "FROM code_tree ct "
-              + "  JOIN code c ON (ct.id = c.parent_id) "
+              + "  SELECT id root_id, 0 level, code.* "
+              + "  FROM entity_version_codes "
+              + "    JOIN code ON (codes_id = code.id) "
+              + "  WHERE entity_version_id = :entityVersionId "
+              + "  UNION ALL "
+              + "  SELECT root_id, level + 1 level, c.* "
+              + "  FROM code_tree ct "
+              + "    JOIN code c ON (ct.id = c.parent_id) "
               + ") "
               + "SELECT * "
-              + "FROM entity_version_codes "
-              + "  JOIN code_tree ON (codes_id = root_id) "
-              + "WHERE entity_version_id = :entityVersionId "
-              + "  AND CASE WHEN :levelCap IS NULL THEN true ELSE level <= :levelCap END ")
+              + "FROM code_tree "
+              + "WHERE CASE WHEN :levelCap IS NULL THEN true ELSE level <= :levelCap END ")
   List<CodeDao> getCodeTreeByEntityVersionId(Long entityVersionId, Integer levelCap);
 }
