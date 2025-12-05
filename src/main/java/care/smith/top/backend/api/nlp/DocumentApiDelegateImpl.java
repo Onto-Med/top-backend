db784fd7c8da4c2087df9846488842bdf1a6ac7e
Franz Matthies <fmatthies@users.noreply.github.com>
2025-12-05 10:32:36 +0100

    Implemented 'importDocuments' method (#272)
    * Implement 'importDocuments' method
    
    * Restrict importDocuments to ADMIN
    
    * Add env variable for max combined document upload size
    
    * Catch ES error for when index was not found to give this info back to frontend

--- src/main/java/care/smith/top/backend/api/nlp/DocumentApiDelegateImpl.java
+++ src/main/java/care/smith/top/backend/api/nlp/DocumentApiDelegateImpl.java
@@ -8,17 +8,21 @@ import care.smith.top.backend.util.nlp.DocumentRepresentation;
 import care.smith.top.backend.util.nlp.NLPUtils;
 import care.smith.top.model.Document;
 import care.smith.top.model.DocumentGatheringMode;
+import care.smith.top.model.DocumentImport;
 import care.smith.top.model.DocumentPage;
 import care.smith.top.top_document_query.adapter.TextAdapter;
 import care.smith.top.top_document_query.elasticsearch.DocumentEntity;
+import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -137,6 +141,13 @@ public class DocumentApiDelegateImpl implements DocumentApiDelegate {
     } catch (IOException e) {
       LOGGER.fine("Server Instance could not be reached/queried.");
       return ResponseEntity.of(Optional.of(new DocumentPage()));
+    } catch (ElasticsearchException e) {
+      LOGGER.fine("Elasticsearch Server threw an exception: '" + e.getMessage() + "'.");
+      if (Objects.equals(e.response().error().type(), "index_not_found_exception")) {
+        LOGGER.finest("Index Not Found : '" + corpusId + "'.");
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
+      return ResponseEntity.of(Optional.of(new DocumentPage()));
     }
     if (documentPage != null) {
       return ResponseEntity.ok(ApiModelMapper.toDocumentPage(documentPage));
@@ -216,6 +227,24 @@ public class DocumentApiDelegateImpl implements DocumentApiDelegate {
       LOGGER.fine("Server Instance could not be reached/queried.");
       return ResponseEntity.ok(new DocumentPage());
     }
+  }
+
+  @Override
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<DocumentImport> importDocuments(
+      String dataSource, String language, List<@Valid Document> documents) {
+    try {
+      TextAdapter adapter = documentService.getAdapterForDataSource(dataSource);
+      DocumentImport importResult =
+          adapter.importDocuments(documents.toArray(new Document[0]), language);
+      return ResponseEntity.ok(importResult);
+    } catch (InstantiationException e) {
+      LOGGER.severe("The text adapter '" + dataSource + "' could not be initialized.");
+    } catch (IOException e) {
+      LOGGER.severe(
+          "Server Instance could not be reached/queried for datasource '" + dataSource + "'.");
+    }
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
   }
 
   private String[] parseHighlightString(String highlightString) {
